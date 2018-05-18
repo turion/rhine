3e7a669f78170779e9fe7ebae7e4c5829f0e3dd6
Manuel Bärenz <maths@manuelbaerenz.de>
2018-05-18 21:29:25 +0200

    Some more ideas that don't seem to fix the leak

--- rhine-examples/src/SpaceLeak.hs
+++ rhine-examples/src/SpaceLeak.hs
@@ -1,7 +1,13 @@
+-- base
+import Data.List
+
+-- rhine
 import FRP.Rhine
 import FRP.Rhine.Clock.Realtime.Busy
 import FRP.Rhine.Clock.Realtime.Stdin
+import FRP.Rhine.ResamplingBuffer.Collect
 import FRP.Rhine.ResamplingBuffer.Timeless
+import FRP.Rhine.ResamplingBuffer.Util
 import FRP.Rhine.Schedule.Concurrently
 
 rb :: (Monad m, Num a) => ResamplingBuffer m cla clb a a
@@ -10,6 +16,7 @@ rb = timelessResamplingBuffer AsyncMealy
   , amGet = \  s -> return (s, s)
   } 0
 
+rb' :: (Monad m, Num a) => ResamplingBuffer m cla clb a a
 rb' = go 0
   where
     go s = ResamplingBuffer
@@ -17,8 +24,14 @@ rb' = go 0
       , get = \_   -> return (s, go s)
     }
 
+rb'' :: (Monad m, Num a) => ResamplingBuffer m cla clb a a
+rb'' = pureBuffer $ foldl' (+) 0
+
+rb''' :: (Monad m, Num a) => ResamplingBuffer m cla clb a a
+rb''' = collect >>-^ arr (foldl' (+) 0)
+
 thing1 = timeInfoOf sinceStart >>> arr sin
 
 main = do
   putStrLn "Press return to force the buffer"
-  flow $ thing1 @@ Busy >-- rb' -@- concurrently --> arrMSync print @@ StdinClock
+  flow $ thing1 @@ Busy >-- rb -@- concurrently --> arrMSync print @@ StdinClock
