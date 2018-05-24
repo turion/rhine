30a8af12c92082a732d1fd30bc975eae524d3879
Manuel Bärenz <maths@manuelbaerenz.de>
2018-05-24 20:29:57 +0200

    Haddock fix

--- rhine/src/FRP/Rhine/SyncSF/Except/Util.hs
+++ rhine/src/FRP/Rhine/SyncSF/Except/Util.hs
@@ -7,6 +7,6 @@ import Control.Monad.Trans.Except
 import Control.Monad.Trans.Reader
 
 
--- | Commute the effects of the |ReaderT| and the |ExceptT| monad.
+-- | Commute the effects of the 'ReaderT' and the 'ExceptT' monad.
 commuteReaderExcept :: ReaderT r (ExceptT e m) a -> ExceptT e (ReaderT r m) a
 commuteReaderExcept a = ExceptT $ ReaderT $ \r -> runExceptT $ runReaderT a r
