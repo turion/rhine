df2c1b8dbff3d13d7f6cc6422b71dd260e59ae39
Manuel Bärenz <maths@manuelbaerenz.de>
2018-05-25 18:18:34 +0200

    Renamed Step -> FixedStep. Refs #45.

--- rhine/src/FRP/Rhine/Clock/Realtime/Millisecond.hs
+++ rhine/src/FRP/Rhine/Clock/Realtime/Millisecond.hs
@@ -17,7 +17,7 @@ import Data.Vector.Sized (Vector, fromList)
 
 -- rhine
 import FRP.Rhine
-import FRP.Rhine.Clock.Step
+import FRP.Rhine.Clock.FixedStep
 import FRP.Rhine.ResamplingBuffer.Collect
 import FRP.Rhine.ResamplingBuffer.Util
 
@@ -32,7 +32,7 @@ The tag of this clock is 'Bool',
 where 'True' represents successful realtime,
 and 'False' a lag.
 -}
-newtype Millisecond (n :: Nat) = Millisecond (RescaledClockS IO (Step n) UTCTime Bool)
+newtype Millisecond (n :: Nat) = Millisecond (RescaledClockS IO (FixedStep n) UTCTime Bool)
 -- TODO Consider changing the tag to Maybe Double
 
 instance Clock IO (Millisecond n) where
@@ -46,9 +46,9 @@ instance Clock IO (Millisecond n) where
 --   Consequently, the tag is constantly 'False',
 --   since the clock will accumulate the computation time as lag.
 sleepClock :: KnownNat n => Millisecond n
-sleepClock = sleepClock_ Step
+sleepClock = sleepClock_ FixedStep
   where
-    sleepClock_ :: Step n -> Millisecond n
+    sleepClock_ :: FixedStep n -> Millisecond n
     sleepClock_ cl = Millisecond $ RescaledClockS cl $ const $ do
       now <- getCurrentTime
       let ticks = arrM_ $ do
@@ -67,7 +67,7 @@ sleepClock = sleepClock_ Step
 --   If the next tick should already have occurred,
 --   the tag is set to 'False', representing a failed real time attempt.
 waitClock :: KnownNat n => Millisecond n
-waitClock = Millisecond $ RescaledClockS Step $ \_ -> do
+waitClock = Millisecond $ RescaledClockS FixedStep $ \_ -> do
   initTime <- getCurrentTime
   let
     runningClock = arrM $ \(n, ()) -> do
@@ -82,7 +82,7 @@ waitClock = Millisecond $ RescaledClockS Step $ \_ -> do
   return (runningClock, initTime)
 
 
--- TODO It would be great if this could be directly implemented in terms of downsampleStep
+-- TODO It would be great if this could be directly implemented in terms of downsampleFixedStep
 downsampleMillisecond
   :: (KnownNat n, Monad m)
   => ResamplingBuffer m (Millisecond k) (Millisecond (n * k)) a (Vector n a)
@@ -99,4 +99,4 @@ scheduleMillisecond :: Schedule IO (Millisecond n1) (Millisecond n2)
 scheduleMillisecond = Schedule initSchedule'
   where
     initSchedule' (Millisecond cl1) (Millisecond cl2)
-      = initSchedule (rescaledScheduleS scheduleStep) cl1 cl2
+      = initSchedule (rescaledScheduleS scheduleFixedStep) cl1 cl2
