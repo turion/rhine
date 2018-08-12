db15d772650570c232e69c5c589f7e803fb8125e
Manuel Bärenz <programming@manuelbaerenz.de>
2018-08-12 13:54:11 +0200

    Added GlossSimWithEventClock
    With this clock, it is possible to tick the simulation when an event
    occurs.

--- rhine-gloss/src/FRP/Rhine/Gloss/Internals.hs
+++ rhine-gloss/src/FRP/Rhine/Gloss/Internals.hs
@@ -76,6 +76,52 @@ withProperSimClock clsf = readerS
       let sinceInit = absolute
       returnA          -< TimeInfo { tag = (), .. }
 
+
+-- | Like 'GlossClock',
+--   but the simulation subsystem is also called when an 'Event' occurs.
+type GlossSimWithEventClock event
+  = ParallelClock Identity
+      (RescaledClock (SelectClock GlossEventClock event) Float)
+      GlossSimulationClock
+
+-- | Like 'GlossClock',
+--   but the simulation subsystem is also called when an 'Event' occurs.
+--   Additionally, it only shows the time /differences/ in the tag (or the event).
+--   Usually, you don't need this clock, but rather 'GlossSimWithEventClock'.
+type GlossSimWithEventClock_ event
+  = ParallelClock Identity
+      (SelectClock GlossEventClock event)
+      GlossSimulationClock_
+
+-- | Create a 'GlossSimWithEventClock_' for a given event selector.
+--   You will usually never need this.
+glossSimWithEventClock_
+  :: (Event -> Maybe event) -- ^ The event selector
+  -> GlossSimWithEventClock_ event
+glossSimWithEventClock_ select = ParallelClock
+  { parallelCl1      = SelectClock { mainClock = GlossEventClock, .. }
+  , parallelCl2      = GlossSimulationClock_
+  , parallelSchedule = glossSimWithEventSchedule
+  }
+
+-- | Like 'withProperSimClock', but for 'GlossSimWithEventClock_'.
+withProperSimEventClock
+  :: Monad m
+  => ClSF m (GlossSimWithEventClock  event) a b
+  -> ClSF m (GlossSimWithEventClock_ event) a b
+withProperSimEventClock clsf = readerS
+  $ (intermingle *** Category.id) >>> runReaderS clsf
+  where
+    intermingle :: Monad m => MSF m (TimeInfo (GlossSimWithEventClock_ event)) (TimeInfo (GlossSimWithEventClock event))
+    intermingle = proc TimeInfo { tag = tag' } -> do
+      let (sinceLast, tag) = case tag' of
+            Left event      -> (0        , Left event)
+            Right sinceLast -> (sinceLast, Right   ())
+      absolute <- sumS -< sinceLast
+      let sinceInit = absolute
+      returnA          -< TimeInfo { .. }
+
+
 -- | The clock that ticks for every @gloss@ graphics output.
 data GlossGraphicsClock = GlossGraphicsClock
 
@@ -85,5 +131,9 @@ instance Clock m GlossGraphicsClock where
   initClock _ = error errMsg
 
 -- | A schedule you can't actually use, for internal purposes.
-glossSchedule :: Schedule Identity (SelectClock GlossEventClock a) GlossSimulationClock_
+glossSchedule :: Schedule Identity (SelectClock GlossEventClock event) GlossSimulationClock_
 glossSchedule = error errMsg
+
+-- | Like 'glossSchedule', but for 'GlossSimWithEventClock'.
+glossSimWithEventSchedule :: Schedule Identity (SelectClock GlossEventClock event) GlossSimulationClock_
+glossSimWithEventSchedule = error errMsg
