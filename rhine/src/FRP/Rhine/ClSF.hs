3c4e4c9362fe35e0cd31cee09ec61ae3b7012ef4
Manuel Bärenz <programming@manuelbaerenz.de>
2018-08-12 14:47:12 +0200

    Added MonadRandom support for ClSFs

--- rhine/src/FRP/Rhine/ClSF.hs
+++ rhine/src/FRP/Rhine/ClSF.hs
@@ -14,5 +14,6 @@ module FRP.Rhine.ClSF ( module X ) where
 -- rhine
 import FRP.Rhine.ClSF.Core   as X
 import FRP.Rhine.ClSF.Except as X
+import FRP.Rhine.ClSF.Random as X
 import FRP.Rhine.ClSF.Reader as X
 import FRP.Rhine.ClSF.Util   as X
