450c15d33b491a5cc865b35b6db04a5b0d01cccb
Manuel Bärenz <programming@manuelbaerenz.de>
2018-09-25 22:42:45 +0200

    Complete and clean up ClSF.Except. Fixes #20.

--- rhine/src/FRP/Rhine/ClSF/Except.hs
+++ rhine/src/FRP/Rhine/ClSF/Except.hs
@@ -16,6 +16,9 @@ module FRP.Rhine.ClSF.Except
   )
   where
 
+-- base
+import qualified Control.Category as Category
+
 -- transformers
 import Control.Monad.Trans.Class (lift)
 import Control.Monad.Trans.Except as X
@@ -31,10 +34,56 @@ import qualified Control.Monad.Trans.MSF.Except as MSFE
 import FRP.Rhine.ClSF.Core
 import FRP.Rhine.ClSF.Except.Util
 
--- * Types
+-- * Throwing exceptions
+
+
+-- | Immediately throw the incoming exception.
+throwS :: Monad m => ClSF (ExceptT e m) cl e a
+throwS = arrMCl throwE
+
+-- | Immediately throw the given exception.
+throw :: Monad m => e -> MSF (ExceptT e m) a b
+throw = arrM_ . throwE
+
+-- | Do not throw an exception.
+pass :: Monad m => MSF (ExceptT e m) a a
+pass = Category.id
+
+-- | Throw the given exception when the 'Bool' turns true.
+throwOn :: Monad m => e -> ClSF (ExceptT e m) cl Bool ()
+throwOn e = proc b -> throwOn' -< (b, e)
+
+-- | Variant of 'throwOn', where the exception can vary every tick.
+throwOn' :: Monad m => ClSF (ExceptT e m) cl (Bool, e) ()
+throwOn' = proc (b, e) -> if b
+  then throwS  -< e
+  else returnA -< ()
+
+-- | Throw the exception 'e' whenever the function evaluates to 'True'.
+throwOnCond :: Monad m => (a -> Bool) -> e -> ClSF (ExceptT e m) cl a a
+throwOnCond cond e = proc a -> if cond a
+  then throwS  -< e
+  else returnA -< a
+
+-- | Variant of 'throwOnCond' for Kleisli arrows.
+-- | Throws the exception when the input is 'True'.
+throwOnCondM :: Monad m => (a -> m Bool) -> e -> ClSF (ExceptT e m) cl a a
+throwOnCondM cond e = proc a -> do
+  b <- arrMCl (lift . cond) -< a
+  if b
+    then throwS  -< e
+    else returnA -< a
+
+-- | When the input is @Just e@, throw the exception @e@.
+throwMaybe :: Monad m => ClSF (ExceptT e m) cl (Maybe e) (Maybe a)
+throwMaybe = proc me -> case me of
+  Nothing -> returnA -< Nothing
+  Just e  -> throwS  -< e
+
+-- * Monad interface
 
 {- | A synchronous exception-throwing signal function.
-It is based on a @newtype@, 'MSFExcept',
+It is based on a @newtype@ from Dunai, 'MSFExcept',
 to exhibit a monad interface /in the exception type/.
 `return` then corresponds to throwing an exception,
 and `(>>=)` is exception handling.
@@ -48,7 +97,8 @@ and `(>>=)` is exception handling.
 -}
 type ClSFExcept m cl a b e = MSFExcept (ReaderT (TimeInfo cl) m) a b e
 
-{- | A clock polymorphic 'ClSFExcept'.
+{- | A clock polymorphic 'ClSFExcept',
+or equivalently an exception-throwing behaviour.
 Any clock with time domain @time@ may occur.
 -}
 type BehaviourFExcept m time a b e
@@ -57,10 +107,6 @@ type BehaviourFExcept m time a b e
 -- | Compatibility to U.S. american spelling.
 type BehaviorFExcept m time a b e = BehaviourFExcept m time a b e
 
-
--- | Commute a 'ReaderT' layer past an 'ExceptT' layer.
-commuteExceptReader :: ExceptT e (ReaderT r m) a -> ReaderT r (ExceptT e m) a
-commuteExceptReader a = ReaderT $ \r -> ExceptT $ runReaderT (runExceptT a) r
 
 -- | Leave the monad context, to use the 'ClSFExcept' as an 'Arrow'.
 runClSFExcept :: Monad m => ClSFExcept m cl a b e -> ClSF (ExceptT e m) cl a b
@@ -81,25 +127,6 @@ once f = MSFE.once $ lift . f
 once_ :: Monad m => m e -> ClSFExcept m cl a b e
 once_ = once . const
 
--- | Immediately throw the exception on the input.
-throwS :: Monad m => ClSF (ExceptT e m) cl e a
-throwS = arrMCl throwE
-
--- | Throw the given exception when the 'Bool' turns true.
-throwOn :: Monad m => e -> ClSF (ExceptT e m) cl Bool ()
-throwOn e = proc b -> throwOn' -< (b, e)
-
--- | Variant of 'throwOn', where the exception can vary every tick.
-throwOn' :: Monad m => ClSF (ExceptT e m) cl (Bool, e) ()
-throwOn' = proc (b, e) -> if b
-  then throwS  -< e
-  else returnA -< ()
-
--- | When the input is @Just e@, throw the exception @e@.
-throwMaybe :: Monad m => ClSF (ExceptT e m) cl (Maybe e) (Maybe a)
-throwMaybe = proc me -> case me of
-  Nothing -> returnA -< Nothing
-  Just e  -> throwS  -< e
 
 -- | Advances a single tick with the given Kleisli arrow,
 --   and then throws an exception.
