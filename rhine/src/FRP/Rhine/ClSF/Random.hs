4b621ba9937cdfab585565c42330b6500e563003
Manuel Bärenz <m.baerenz@sonnen.de>
2018-09-27 18:58:56 +0200

    Complete ClSF.Random

--- rhine/src/FRP/Rhine/ClSF/Random.hs
+++ rhine/src/FRP/Rhine/ClSF/Random.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeFamilies #-}
 -- | Create 'ClSF's with randomness without 'IO'.
 --   Uses the @MonadRandom@ package.
 --   This module copies the API from @dunai@'s
@@ -11,22 +13,24 @@ module FRP.Rhine.ClSF.Random
 
 
 -- transformers
-import Control.Monad.Trans.Reader
+import Control.Monad.IO.Class
+
+-- random
+import System.Random (newStdGen)
 
 -- MonadRandom
 import Control.Monad.Random
 
 -- dunai
+import Control.Monad.Trans.MSF.Except (performOnFirstSample)
 import qualified Control.Monad.Trans.MSF.Random as MSF
-import Control.Monad.Trans.MSF.Random as X hiding (runRandS, evalRandS)
+import Control.Monad.Trans.MSF.Random as X hiding (runRandS, evalRandS, getRandomS, getRandomRS, getRandomRS_)
 
 -- rhine
 import FRP.Rhine.ClSF.Core
+import FRP.Rhine.ClSF.Random.Util
 
-
--- | Commute one 'ReaderT' layer past a 'RandT' layer.
-commuteReaderRand :: ReaderT r (RandT g m) a -> RandT g (ReaderT r m) a
-commuteReaderRand (ReaderT f) = liftRandT $ \g -> ReaderT $ \r -> runRandT (f r) g
+-- * Generating random values from the 'RandT' transformer
 
 -- | Generates random values, updating the generator on every step.
 runRandS
@@ -43,3 +47,51 @@ evalRandS
   -> g
   -> ClSF          m  cl a b
 evalRandS clsf g = runRandS clsf g >>> arr snd
+
+-- | Updates the generator every step but discards the value,
+--   only outputting the generator.
+execRandS
+  :: (RandomGen g, Monad m)
+  => ClSF (RandT g m) cl a b
+  -> g
+  -> ClSF          m  cl a g
+execRandS clsf g = runRandS clsf g >>> arr fst
+
+-- | Evaluates the random computation by using the global random generator.
+evalRandIOS
+  :: Monad m
+  =>     ClSF (RandT StdGen m) cl a b
+  -> IO (ClSF               m  cl a b)
+evalRandIOS clsf = do
+  g <- newStdGen
+  return $ evalRandS clsf g
+
+-- | Evaluates the random computation by using the global random generator on the first tick.
+evalRandIOS'
+  :: MonadIO m
+  => ClSF (RandT StdGen m) cl a b
+  -> ClSF               m  cl a b
+evalRandIOS' = performOnFirstSample . liftIO . evalRandIOS
+
+-- * Creating random behaviours
+
+-- | Produce a random value at every tick.
+getRandomS
+  :: (MonadRandom m, Random a)
+  => Behaviour m time a
+getRandomS = constMCl getRandom
+
+-- | Produce a random value at every tick,
+--   within a range given per tick.
+getRandomRS
+  :: (MonadRandom m, Random a)
+  => BehaviourF m time (a, a) a
+getRandomRS = arrMCl getRandomR
+
+-- | Produce a random value at every tick,
+--   within a range given once.
+getRandomRS_
+  :: (MonadRandom m, Random a)
+  => (a, a)
+  -> Behaviour m time a
+getRandomRS_ = constMCl . getRandomR
