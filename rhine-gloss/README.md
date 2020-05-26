aae19a377e8760bbc0134ace70912eba9631b939
Manuel Bärenz <m.baerenz@sonnen.de>
2020-05-26 19:33:59 +0200

    Rework Gloss backends

--- rhine-gloss/README.md
+++ rhine-gloss/README.md
@@ -8,6 +8,8 @@ or `stack build && stack exec rhine-gloss-gears`,
 now becomes as simple as:
 
 ```haskell
+module README where
+
 import FRP.Rhine.Gloss
 
 
@@ -18,12 +20,11 @@ gears angle = color green $ pictures
   : map (rotate angle) [ rotate (45 * n) $ rectangleSolid 20 150 | n <- [0..3] ]
 
 -- | Rotate the gear with a constant angular velocity.
-mainClSF :: GlossClSF a
+mainClSF :: GlossClSF
 mainClSF = timeInfoOf sinceInit >>> arr (* 50) >>> arr gears
 
 main :: IO ()
-main = flowGloss (InWindow "rhine-gloss-gears" (400, 400) (10, 10)) (greyN 0.3) 30
-     $ buildGlossRhine Just mainClSF
+main = flowGloss defaultSettings mainClSF
 ```
 
 ## Installation
--- rhine-gloss/src/FRP/Rhine/Gloss.hs
+++ rhine-gloss/src/FRP/Rhine/Gloss.hs
@@ -1,95 +1,23 @@
-{- | Wrapper library to write Gloss applications in Rhine.
-@gloss@ acts as the backend here.
+{- | Wrapper library to write @gloss@ Gloss applications in Rhine.
 
-A Rhine app with the Gloss backend must use the 'GlossClock',
+A pure Rhine app with @gloss@ backend must use the 'GlossClock' or 'GlossCombinedClock'
+(from 'FRP.Rhine.Gloss.Pure.Single' and 'FRP.Rhine.Gloss.Pure.Combined', respectively),
 since the @gloss@ API only offers callbacks.
-In order to run such a reactive program, you have to use 'flowGloss'.
+In order to run such a reactive program, you have to use 'flowGloss' or 'flowGlossCombined'.
+
+A more flexible alternative, at the cost of introducing 'IO' concurrency,
+is the 'FRP.Rhine.Gloss.IO' wrapper.
+There, you can combine the @gloss@ clocks with arbitrary other 'IO' clocks.
 -}
-{-# LANGUAGE Arrows #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE TypeFamilies #-}
-module FRP.Rhine.Gloss
-  ( module FRP.Rhine.Gloss
-  , module X
-  )
-  where
+module FRP.Rhine.Gloss (module X) where
 
--- base
-import Data.Maybe (fromMaybe)
-import Data.Functor.Identity (Identity, runIdentity)
-
-import qualified Control.Arrow as X
-
--- gloss
-import Graphics.Gloss.Interface.Pure.Game
-
-import qualified Graphics.Gloss as X
-
--- dunai
-import Data.MonadicStreamFunction.InternalCore
+import Control.Arrow as X
 
 -- rhine
-import FRP.Rhine
-import FRP.Rhine.Reactimation.ClockErasure
-
-import qualified FRP.Rhine      as X
-import qualified FRP.Rhine.ClSF as X
+import FRP.Rhine as X
 
 -- rhine-gloss
-import FRP.Rhine.Gloss.Internals
-
--- TODO Consider generalising to IO
-
-
--- | The overall clock of a valid @rhine@ 'SN' that can be run by @gloss@.
---   @a@ is the type of subevents that are selected.
-type GlossClock a
-  = SequentialClock Identity
-      (SelectClock GlossEventClock a)
-      GlossSimulationClock_
-
--- | The type of a valid 'Rhine' that can be run by @gloss@.
---   @a@ is the type of subevents that are selected.
-type GlossRhine a = Rhine Identity (GlossClock a) () Picture
-
--- | The type of a 'ClSF' that you have to implement to get a @gloss@ app.
-type GlossClSF a = ClSF Identity GlossSimulationClock [a] Picture
-
-{- | For most applications, it is sufficient to implement
-a single signal function
-that is called with a list of all relevant events
-that occurred in the last tick.
--}
-buildGlossRhine
-  :: (Event -> Maybe a) -- ^ The event selector
-  -> GlossClSF a        -- ^ The 'ClSF' representing the game loop.
-  -> GlossRhine a
-buildGlossRhine select clsfSim
-  =   timeInfoOf tag @@  SelectClock { mainClock = GlossEventClock, .. }
-  >-- collect -@- glossSchedule
-  --> withProperSimClock clsfSim @@ GlossSimulationClock_
-
--- | The main function that will start the @gloss@ backend and run the 'SN'.
-flowGloss
-  :: Display      -- ^ Display mode (e.g. 'InWindow' or 'FullScreen').
-  -> Color        -- ^ Background color.
-  -> Int          -- ^ Number of simulation steps per second of real time.
-  -> GlossRhine a -- ^ The @gloss@-compatible 'Rhine'.
-  -> IO ()
-flowGloss display color n Rhine {..}
-  = play display color n worldMSF getPic handleEvent simStep
-  where
-    worldMSF = feedback Blank $ proc (input, lastPic) -> do
-      case input of
-        Just usual -> do
-          maybeNewPic <- eraseClockSN () sn -< usual
-          let newPic = fromMaybe lastPic maybeNewPic
-          returnA -< (newPic, newPic)
-        Nothing -> returnA -< (lastPic, lastPic)
-    getPic msf = fst $ runIdentity $ unMSF msf Nothing
-    handleEvent event msf = case select (sequentialCl1 clock) event of
-      Just a  -> snd $ runIdentity $ unMSF msf $ Just ((), Left a, Just ()) -- Event is relevant
-      Nothing -> msf -- Event is irrelevant, state doesn't change
-    simStep diff msf = snd $ runIdentity $ unMSF msf $ Just ((), Right diff, Nothing)
+import FRP.Rhine.Gloss.IO            as X
+import FRP.Rhine.Gloss.Common        as X
+import FRP.Rhine.Gloss.Pure          as X
+import FRP.Rhine.Gloss.Pure.Combined as X
