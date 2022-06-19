1fe4b4fae8bf9989522478eac6acfb444ac4ed4e
Jun Matsushita <jun@iilab.org>
2022-06-19 19:58:16 +0200

    Add more features to repl example

--- rhine-terminal/Main.hs
+++ rhine-terminal/Main.hs
@@ -26,6 +26,7 @@ import FRP.Rhine
 import System.IO hiding (putChar)
 
 import FRP.Rhine.Terminal (TerminalEventClock(..), terminalConcurrently)
+import System.Exit (exitSuccess)
 
 
 type App = AppT IO
@@ -33,53 +34,95 @@ type AppT = TerminalT LocalTerminal
 
 -- Clocks
 
-type KeyClock = SelectClock (TerminalEventClock LocalTerminal) Char
+data Input = Char Char | Space | Backspace | Enter
 
-keyClock :: LocalTerminal -> KeyClock
-keyClock term = SelectClock { mainClock = TerminalEventClock term, select = selectChar }
+type InputClock = SelectClock (TerminalEventClock LocalTerminal) Input
+
+keyClock :: LocalTerminal -> InputClock
+keyClock term = SelectClock { mainClock = TerminalEventClock term, select = selectKey }
   where
-    selectChar :: Tag (TerminalEventClock LocalTerminal) -> Maybe Char
-    selectChar = \case
-      Right (KeyEvent (CharKey k) _) -> Just k
+    selectKey :: Tag (TerminalEventClock LocalTerminal) -> Maybe Input
+    selectKey = \case
+      Right (KeyEvent (CharKey k) _) -> Just (Char k)
+      Right (KeyEvent SpaceKey _) -> Just Space
+      Right (KeyEvent BackspaceKey _) -> Just Backspace
+      Right (KeyEvent EnterKey _) -> Just Enter
       _ -> Nothing
 
-type BeatClock = Millisecond 1000
+type SignalClock = SelectClock (TerminalEventClock LocalTerminal) Interrupt
 
--- ClSFs
+signalClock :: LocalTerminal -> SignalClock
+signalClock term = SelectClock { mainClock = TerminalEventClock term, select = selectInterrupt }
+  where
+    selectInterrupt :: Tag (TerminalEventClock LocalTerminal) -> Maybe Interrupt
+    selectInterrupt = \case
+      Left i -> Just i
+      _ -> Nothing
 
-beat :: MonadScreen m => ClSF m (LiftClock IO AppT BeatClock) () ()
-beat = (flip T.cons " > " . (cycle " ." !!) <$> count) >-> displayBeat
+type BeatClock = LiftClock IO AppT (Millisecond 1000)
 
-displayBeat :: MonadScreen m => ClSF m cl Text ()
-displayBeat = arrMCl $ \prompt ->  do
-  Position _ column <- getCursorPosition
-  if column /= 0 then do
-    moveCursorBackward column
-    putText prompt
-    setCursorColumn column
-  else putText prompt
-  flush
 
-key :: MonadScreen m => ClSF m KeyClock () ()
-key = tagS >-> displayKey
-
-displayKey :: MonadScreen m => ClSF m KeyClock Char ()
-displayKey = arrMCl $ \k -> do
-  putChar k
-  flush
+type AppClock = ParallelClock App (ParallelClock App InputClock BeatClock) SignalClock
 
 -- Rhines
 
-type DisplayClock = ParClock App (LiftClock IO AppT BeatClock)  KeyClock
+keySource :: LocalTerminal -> Rhine App InputClock () Input
+keySource term = tagS @@ keyClock term
 
-mainRhine :: LocalTerminal -> Rhine App DisplayClock () ()
-mainRhine term = beatRhine ||@ terminalConcurrently term @|| keyRhine
-  where
-    beatRhine :: Rhine App (LiftClock IO (TerminalT LocalTerminal) BeatClock) () ()
-    beatRhine = beat @@ liftClock waitClock
+beatSource :: Rhine App BeatClock () Text
+beatSource = (flip T.cons " > " . (cycle " ." !!) <$> count) @@ liftClock waitClock
 
-    keyRhine :: Rhine App KeyClock () ()
-    keyRhine = key @@ keyClock term
+signalSource :: LocalTerminal ->Rhine App SignalClock () Interrupt
+signalSource term = tagS @@ signalClock term
+
+data Actions = Input Input
+             | Prompt Text
+             | Signal Interrupt
+
+sources :: LocalTerminal
+        -> Rhine App InputClock () Input
+        -> Rhine App BeatClock () Text
+        -> Rhine App SignalClock () Interrupt
+        -> Rhine App AppClock () Actions
+sources term input prompt signal =
+  ( ( input ++@ terminalConcurrently term @++ prompt )
+            ++@ terminalConcurrently term @++ signal
+  ) @>>^ \case
+            Left (Left i) -> Input i
+            Left (Right p) -> Prompt p
+            Right s -> Signal s
+
+-- ClSFs
+
+display :: ClSF App cl Actions ()
+display = arrMCl $ \case
+  Input i ->
+    case i of
+      Char c -> putChar c >> flush
+      Space -> putChar ' ' >> flush
+      Backspace -> moveCursorBackward 1 >> deleteChars 1 >> flush
+      Enter -> putLn >> flush
+
+  Prompt prmpt -> do
+    Position _ column <- getCursorPosition
+    if column /= 0 then do
+      moveCursorBackward column
+      putText prmpt
+      setCursorColumn column
+    else putText prmpt
+    flush
+
+  Signal i -> do
+    putLn
+    putStringLn $ show i ++ ": exiting program."
+    flush
+    liftIO exitSuccess
+
+
+-- Rhines
+
+mainRhine :: LocalTerminal -> Rhine App AppClock () ()
+mainRhine term = sources term (keySource term) beatSource (signalSource term) @>-^ display
 
 -- Main
 
@@ -87,37 +130,6 @@ main :: IO ()
 main = do
   hSetBuffering stdin NoBuffering
   hSetBuffering stdout NoBuffering
-  -- withTerminal $ \term -> runTerminalT (flow $ mainRhine term) term
-  withTerminal $ \term -> runTerminalT (flow $ mainRhine' term) term
+  withTerminal $ \term -> runTerminalT (flow $ mainRhine term) term
 
-------------
---
--- Alternate
 
-beat' :: ClSF App cl () Text
-beat' = flip T.cons " > " . (cycle " ." !!) <$> count
-
-key' :: ClSF App KeyClock () Char
-key' = tagS
-
-display' :: ClSF App cl (Either Text Char) ()
-display' = arrMCl $ \case
-  Left prompt -> do
-    Position _ column <- getCursorPosition
-    if column /= 0 then do
-      moveCursorBackward column
-      putText prompt
-      setCursorColumn column
-    else putText prompt
-    flush
-  Right k -> do
-    putChar k
-    flush
-
--- Rhine of sources
-
-sensor' :: LocalTerminal -> Rhine App DisplayClock () (Either Text Char)
-sensor' term = beat' @@ liftClock waitClock ++@ terminalConcurrently term @++ key' @@ keyClock term
-
-mainRhine' :: LocalTerminal -> Rhine App DisplayClock () ()
-mainRhine' term = sensor' term @>-^ display'
