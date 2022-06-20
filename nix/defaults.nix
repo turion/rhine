7b68e77251a58afa6d62240ae41b59300dd2e898
Jun Matsushita <jun@iilab.org>
2022-06-20 10:16:45 +0200

    Flake (#164)
    Simple flake for multi cabal home
    
    Uses haskell-flake-utils (experimental) to provide build and devShell with nix flakes:
    ```
    nix build .#rhine-gloss
    result/bin/rhine-gloss-gears
    ```
    
    ```
    nix develop
    cabal build rhine
    ```
    
    Co-authored-by: Manuel Bärenz <programming@manuelbaerenz.de>

--- nix/defaults.nix
+++
@@ -1,6 +0,0 @@
-# Sets default versions for global variables
-rec {
-  compiler = "ghc8101";
-  nixpkgs = import <nixpkgs> {};
-  inherit (nixpkgs) pkgs lib;
-}
--- shell.nix
+++ shell.nix
@@ -1,4 +1,9 @@
 let
-  mypkgs = import ./. {};
-  shellFrom = import ./nix/shellFrom.nix {};
-in shellFrom mypkgs.rhine-gloss
+  lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+  flake-compat-src = fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+    sha256 = lock.nodes.flake-compat.locked.narHash;
+  };
+  flake-compat = import flake-compat-src { src =  ./.; };
+in
+flake-compat.shellNix
