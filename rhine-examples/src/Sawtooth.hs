87554cfcf6186bed50ae69f55162951790bcccb0
Manuel Bärenz <programming@manuelbaerenz.de>
2022-06-27 11:53:49 +0200

    Silence further warnings

--- rhine-examples/src/Sawtooth.hs
+++ rhine-examples/src/Sawtooth.hs
@@ -3,12 +3,15 @@ Generate a simple sawtooth signal using exceptions and 'sinceStart'.
 -}
 {-# LANGUAGE Arrows #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
 import FRP.Rhine
 
+sawtooth :: Monad m => Behaviour m UTCTime Double
 sawtooth = safely $ do
   try $ sinceStart >>> proc time -> do
     throwOn () -< time > 1
     returnA    -< time
   safe sawtooth
 
+main :: IO ()
 main = flow $ sawtooth >-> arrMCl print @@ (waitClock :: Millisecond 200)
