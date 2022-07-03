e48fb07d17d13bff2404d9eb14aff9c21c5d019a
Jun Matsushita <jun@iilab.org>
2022-07-03 18:13:50 +0200

    Style and formatting

--- rhine-terminal/tests/Main.hs
+++ rhine-terminal/tests/Main.hs
@@ -1,22 +1,29 @@
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE NamedFieldPuns #-}
 module Main where
 
+-- base
 import Prelude hiding (putChar)
-import FRP.Rhine.Terminal
-import System.Terminal
-import System.Terminal.Internal
-import FRP.Rhine
 import GHC.Conc (retry, readTVarIO, atomically)
 import Control.Concurrent (forkIO, threadDelay)
 import Control.Monad (void)
 
---stm
+-- rhine
+import FRP.Rhine
+import FRP.Rhine.Terminal
+
+-- terminal
+import System.Terminal
+import System.Terminal.Internal
+
+-- text
+import Data.Text (singleton)
+
+-- stm
 import Control.Concurrent.STM.TQueue
 
+-- hspec
 import Test.Hspec
-import Data.Text (singleton)
 
 type KeyClock = SelectClock TerminalEventClock Char
 
@@ -24,22 +31,21 @@ keyClock :: KeyClock
 keyClock = SelectClock { mainClock = TerminalEventClock , select }
   where
     select :: Tag TerminalEventClock -> Maybe Char
-    select = \case
-      Right (KeyEvent (CharKey k) _) -> Just k
-      _ -> Nothing
+    select (Right (KeyEvent (CharKey k) _)) = Just k
+    select _                                = Nothing
 
 defaultSettings :: TQueue Event -> VirtualTerminalSettings
 defaultSettings eventQueue = VirtualTerminalSettings
-    { virtualType         = "xterm"
-    , virtualWindowSize   = pure (Size 3 10)
-    , virtualEvent        = readTQueue eventQueue
-    , virtualInterrupt    = retry
-    }
+  { virtualType         = "xterm"
+  , virtualWindowSize   = pure (Size 3 10)
+  , virtualEvent        = readTQueue eventQueue
+  , virtualInterrupt    = retry
+  }
 
 displayDot :: MonadScreen m => ClSF m KeyClock () ()
-displayDot = tagS >-> arrMCl (\_ -> do
+displayDot = constMCl $ do
   putChar '.'
-  flush)
+  flush
 
 testRhine :: Terminal t => Rhine (TerminalT t IO) KeyClock () ()
 testRhine = displayDot @@ keyClock
@@ -51,20 +57,20 @@ charEvent eventQueue terminal char = do
 
 main :: IO ()
 main = hspec $ do
-    describe "rhine-terminal with VirtualTerminal" $ do
-      it "replaces virtual inputs by dots" $ do
-        eventQueue <- newTQueueIO
-        withVirtualTerminal (defaultSettings eventQueue) $ \t -> do
-          void $ liftIO $ forkIO $ flowTerminal t testRhine
-          charEvent eventQueue t '1'
-          threadDelay $ 1000 * 1000
-          charEvent eventQueue t '2'
-          threadDelay $ 1000 * 1000
-          charEvent eventQueue t '3'
-          threadDelay $ 1000 * 1000
-          readTVarIO (virtualWindow t) `shouldReturn` expWindow
-        where
-          expWindow =
-              [ "1.2.3.    "
-              , "          "
-              , "          " ]
+  describe "rhine-terminal with VirtualTerminal" $ do
+    it "replaces virtual inputs by dots" $ do
+      eventQueue <- newTQueueIO
+      withVirtualTerminal (defaultSettings eventQueue) $ \t -> do
+        void $ liftIO $ forkIO $ flowTerminal t testRhine
+        charEvent eventQueue t '1'
+        threadDelay $ 200 * 1000
+        charEvent eventQueue t '2'
+        threadDelay $ 200 * 1000
+        charEvent eventQueue t '3'
+        threadDelay $ 200 * 1000
+        readTVarIO (virtualWindow t) `shouldReturn` expWindow
+      where
+        expWindow =
+            [ "1.2.3.    "
+            , "          "
+            , "          " ]
