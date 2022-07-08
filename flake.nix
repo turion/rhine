a18fd71f2350c5f3972a9b8c65c5eda64756d578
Jun Matsushita <jun@iilab.org>
2022-07-08 18:13:36 +0200

    Depend on haskell-terminal fork for TerminalT constructor

--- flake.nix
+++ flake.nix
@@ -17,24 +17,40 @@
       inputs.flake-utils.follows = "flake-utils";
     };
 
-  };
-
-outputs = { self, nixpkgs, flake-utils, haskell-flake-utils, flake-compat, ... }:
-  haskell-flake-utils.lib.simpleCabalProject2flake {
-    inherit self nixpkgs;
-
-    systems = [
-      # Tested in CI
-      "x86_64-linux"
-      "x86_64-darwin"
-      # Tested by maintainers
-      "aarch64-darwin"
-      # Not sure we can test this in CI
-      "i686-linux"
-    ];
-
-    name = "rhine";
-    packageNames = [ "rhine-gloss" "rhine-terminal" "rhine-examples" ];
+    terminal-src = {
+      url = "github:jmatsushita/haskell-terminal/reader";
+      flake = false;
+    };
 
   };
+
+  outputs = { self, nixpkgs, flake-utils, haskell-flake-utils, flake-compat, terminal-src, ... }:
+    haskell-flake-utils.lib.simpleCabalProject2flake {
+      inherit self nixpkgs;
+
+      systems = [
+        # Tested in CI
+        "x86_64-linux"
+        "x86_64-darwin"
+        # Tested by maintainers
+        "aarch64-darwin"
+        # Not sure we can test this in CI
+        "i686-linux"
+      ];
+
+      name = "rhine";
+      packageNames = [ "rhine-gloss" "rhine-terminal" "rhine-examples" ];
+
+      shellExtBuildInputs = {pkgs}: with pkgs; [
+        haskellPackages.haskell-language-server
+      ];
+
+      hpPreOverrides = { pkgs }: new: old:
+        with pkgs.haskell.lib; with haskell-flake-utils.lib;
+        {
+          monad-schedule = old.callHackageDirect { pkg = "monad-schedule"; ver = "0.1.2.0"; sha256 = "sha256-7zXI37brdrqlgiRo/RseP0iMhBferzDM+Qiu5F1hhus="; } {};
+          terminal = old.callCabal2nix "terminal" "${terminal-src}" {};
+        };
+
+    };
 }
