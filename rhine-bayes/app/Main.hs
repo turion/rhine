c7c3d8ad3024a3660ef70d732c6e31c4f598a24e
Manuel Bärenz <programming@manuelbaerenz.de>
2022-09-18 10:45:23 +0200

    Remove Sequential

--- rhine-bayes/app/Main.hs
+++ rhine-bayes/app/Main.hs
@@ -12,7 +12,6 @@ import Control.Monad.Bayes.Class
 -- FIXME They should implement MMorph
 import Control.Monad.Bayes.Population hiding (hoist)
 import Control.Monad.Bayes.Sampler
-import Control.Monad.Bayes.Sequential
 
 -- rhine
 import FRP.Rhine
@@ -54,8 +53,7 @@ filtered :: (MonadInfer m, Diff td ~ Double) => BehaviourF m td (StdDev, Sensor)
 filtered = bayesFilter model
 
 -- FIXME Want ExceptT so we can exit with escape
-type MySmallMonad = Sequential (GlossConcT SamplerIO)
--- FIXME Don't need Sequential anymore?
+type MySmallMonad = GlossConcT SamplerIO
 
 data Result = Result
   { measured :: Sensor
@@ -78,14 +76,14 @@ filteredAndTrue = proc stdDev -> do
 -- Or rather decouple simulation and graphics, and then make simulation a busy loop (with a little sleep) and display the simulation rate.
 visualisation :: BehaviourF MySmallMonad td Result ()
 visualisation = proc Result { measured, latent, particles } -> do
-  constMCl $ lift clearIO -< ()
+  constMCl clearIO -< ()
   drawBall -< (measured, 0.3, red)
   drawBall -< (latent, 0.3, green)
   drawParticles -< particles
 
 drawBall :: BehaviourF MySmallMonad td (Pos, Double, Color) ()
 drawBall = proc (position, width, theColor) -> do
-  arrMCl $ lift . paintIO -< scale 20 20 $ uncurry translate (double2FloatTuple position) $ color theColor $ circleSolid $ double2Float width
+  arrMCl paintIO -< scale 20 20 $ uncurry translate (double2FloatTuple position) $ color theColor $ circleSolid $ double2Float width
 
 drawParticle :: BehaviourF MySmallMonad td (Pos, Log Double) ()
 drawParticle = proc (position, probability) -> do
@@ -117,9 +115,9 @@ instance MonadSample m => MonadSample (GlossConcT m) where
   random = lift random
   -- FIXME Other PDs?
 
-glossClock :: RescaledClock (LiftClock (GlossConcT SamplerIO) Sequential GlossSimClockIO) Double
+glossClock :: RescaledClock GlossSimClockIO Double
 glossClock = RescaledClock
-  { unscaledClock = liftClock GlossSimClockIO
+  { unscaledClock = GlossSimClockIO
   , rescale = float2Double
   }
 
@@ -128,6 +126,5 @@ main = do
   thing <- sampleIO
     $ launchGlossThread defaultSettings
       { display = InWindow "rhine-bayes" (1024, 960) (10, 10) }
-    $ finish
     $ reactimateCl glossClock mainClSF
   print thing
