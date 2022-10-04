61f40ddcae661205601cb8cab99c4fa047238ca4
Manuel Bärenz <programming@manuelbaerenz.de>
2022-10-04 20:25:39 +0200

    Fun fact this is a GHC 9.0.2 compiler error.

--- rhine/src/FRP/Rhine/SNGeneralization.hs
+++ rhine/src/FRP/Rhine/SNGeneralization.hs
@@ -128,7 +128,7 @@ clockErasure ::
 clockErasure initialTime (Synchronous clsf sn) = case internalClocksMatchTimeDomain sn of
   Refl -> proc (time, tag, input) -> do
     case (analyseTag tag, analyseIn input) of
-      (Left tagClSF, Left a) -> do
+      (TagHere tagClSF, InHere a) -> do
         b <- eraseClockClSF LeafProxy initialTime clsf -< (time, tagClSF, a)
         returnA -< OutHere b
       (Right tagClocks, Right a) -> do
