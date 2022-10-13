f45de7a2f39ce17d67a1f0cfa64473c8775dce06
Manuel Bärenz <programming@manuelbaerenz.de>
2022-10-13 15:32:36 +0200

    WIP

--- rhine/src/FRP/Rhine/SNGeneralization.hs
+++ rhine/src/FRP/Rhine/SNGeneralization.hs
@@ -19,6 +19,8 @@ import FRP.Rhine.Reactimation.ClockErasure (eraseClockClSF, eraseClockResBuf)
 import FRP.Rhine.Clock.Proxy
 import FRP.Rhine.Schedule (In, Out)
 import Data.Void
+import FRP.Rhine.Clock.Util (genTimeInfo)
+import Data.Constraint (Dict (Dict), mapDict, withDict)
 
 data Plug = Closed | Open Type
 data Clocked = Clocked Plug Type Plug
@@ -35,6 +37,13 @@ data SN td (clocks :: [Clocked]) (m :: Type -> Type) where
     ResBuf m cl1 cl2 a b ->
     SN td ('Clocked (Open b) cl2 c ': 'Clocked d cl1 (Open a) ': clocks) m ->
     SN td ('Clocked Closed cl2 c ': 'Clocked d cl1 Closed ': clocks) m
+  ResamplingSomewhere ::
+    (td ~ Time cl1, td ~ Time cl2) =>
+    ResBuf m cl1 cl2 a b ->
+    ClosingIn clocksBefore clocksIntermediate b cl2 ->
+    ClosingOut clocksIntermediate clocksAfter a cl1 ->
+    SN td clocksBefore m ->
+    SN td clocksAfter m
   DoneIn ::
     SN td ('Clocked (Open a) cl b ': clocks) m ->
     SN td ('Clocked Closed cl b ': clocks) m
@@ -43,6 +52,7 @@ data SN td (clocks :: [Clocked]) (m :: Type -> Type) where
     SN td ('Clocked a cl (Open b) ': clocks) m ->
     SN td ('Clocked a cl Closed ': clocks) m
   ConcatInNil ::
+    TimeDomain td =>
     SN td clocks m ->
     SN td ('Clocked (Open (HList '[])) (Clocks td '[]) (Open (HList '[])) ': clocks) m
   ConcatInConsOO ::
@@ -63,12 +73,12 @@ data Clocks (td :: Type) (cls :: [Type]) = Clocks (HList cls)
 instance TimeDomain td => Clock m (Clocks td '[]) where
   type Time (Clocks td '[]) = td
   type Tag (Clocks td '[]) = Void
-  initClock cl = _
+  initClock cl = error "Empty clock doesn't tick"
 
 instance (TimeDomain td, Clock m cl, Clock m (Clocks td cls), Time cl ~ Time (Clocks td cls)) => Clock m (Clocks td (cl ': cls)) where
   type Time (Clocks td (cl ': cls)) = td
   type Tag (Clocks td (cl ': cls)) = Either (Tag cl) (Tag (Clocks td cls))
-  initClock cl = _
+  initClock cl = error "Not yet implemented"
 
 data Permutation (before :: [a]) (after :: [a]) where
   Identity :: Permutation as as
@@ -82,17 +92,52 @@ data Swap (before :: [a]) (after :: [a]) where
 
 deriving instance (Show (Swap before after))
 
+data ClosingIn (clocksBefore :: [Clocked]) (clocksAfter :: [Clocked]) a cl where
+  ClosingInHere :: ClosingIn ('Clocked (Open a) cl b ': cls) ('Clocked Closed cl b ': cls) a cl
+  ClosingInThere :: ClosingIn clocksBefore clocksAfter a cl -> ClosingIn (clocked ': clocksBefore) (clocked ': clocksAfter) a cl
+
+data ClosingOut (clocksBefore :: [Clocked]) (clocksAfter :: [Clocked]) b cl where
+  ClosingOutHere :: ClosingOut ('Clocked a cl (Open b) ': cls) ('Clocked a cl Closed ': cls) b cl
+  ClosingOutThere :: ClosingOut clocksBefore clocksAfter b cl -> ClosingOut (clocked ': clocksBefore) (clocked ': clocksAfter) b cl
+
 data InClocked (inClocks :: [Clocked]) where
   InHere :: a -> InClocked ('Clocked (Open a) cl b ': cls)
+  InNoop :: InClocked ('Clocked Closed cl b ': cls)
   InThere :: InClocked cls -> InClocked ('Clocked a cl b ': cls)
 
-analyseIn :: InClocked ('Clocked (Open a) cl b ': cls) -> Either e (InClocked cls)
+analyseIn :: InClocked ('Clocked (Open a) cl b ': cls) -> Either a (InClocked cls)
 analyseIn (InHere a) = Left a
-analyseIn (InThere cls) = Right $ analyseIn cls
+analyseIn (InThere cls) = Right cls
+
+-- FIXME Rewrite with lenses
+thereIn :: InClocked ('Clocked a cl b ': cls) -> Maybe (InClocked cls)
+thereIn (InHere _) = Nothing
+thereIn InNoop = Nothing
+thereIn (InThere cls) = Just cls
 
 data OutClocked (outClocks :: [Clocked]) where
   OutHere :: b -> OutClocked ('Clocked a cl (Open b) ': cls)
+  OutNoop :: OutClocked ('Clocked a cl Closed ': cls)
   OutThere :: OutClocked cls -> OutClocked ('Clocked a cl b ': cls)
+
+analyseOut :: OutClocked ('Clocked a cl (Open b) ': cls) -> Either b (OutClocked cls)
+analyseOut (OutHere a) = Left a
+analyseOut (OutThere cls) = Right cls
+
+outNow :: OutClocked ('Clocked a cl b ': cls) -> OutClocked ('Clocked a' cl b ': cls')
+outNow (OutHere b) = OutHere b
+outNow OutNoop = OutNoop
+outNow (OutThere _) = error "outNow: OutThere"
+
+thereOut :: OutClocked ('Clocked a cl b ': cls) -> Maybe (OutClocked cls)
+thereOut (OutHere _) = Nothing
+thereOut OutNoop = Nothing
+thereOut (OutThere cls) = Just cls
+
+disregardIn :: OutClocked ('Clocked a cl b ': cls) -> OutClocked ('Clocked a' cl b ': cls)
+disregardIn (OutHere b) = OutHere b
+disregardIn OutNoop = OutNoop
+disregardIn (OutThere cls) = OutThere cls
 
 data TagClocked (clocks :: [Clocked]) where
   TagHere :: Tag cl -> TagClocked ('Clocked a cl b ': cls)
@@ -100,7 +145,7 @@ data TagClocked (clocks :: [Clocked]) where
 
 analyseTag :: TagClocked ('Clocked a cl b ': cls) -> Either (Tag cl) (TagClocked cls)
 analyseTag (TagHere tag) = Left tag
-analyseTag (TagThere cls) = Right $ analyseTag cls
+analyseTag (TagThere cls) = Right cls
 
 -- data ClockedData (clocked :: [Clocked]) where
 --   CDLeft :: a -> ClockedData ('Clocked a cl ': clas)
@@ -124,19 +169,62 @@ clockErasure ::
   SN td clocks m ->
   MSF m
     (td, TagClocked clocks, InClocked clocks)
-    (InClocked clocks)
-clockErasure initialTime (Synchronous clsf sn) = case internalClocksMatchTimeDomain sn of
-  Refl -> proc (time, tag, input) -> do
-    case (analyseTag tag, analyseIn input) of
-      (Left tagClSF, Left a) -> do
-        b <- eraseClockClSF LeafProxy initialTime clsf -< (time, tagClSF, a)
-        returnA -< OutHere b
-      (Right tagClocks, Right a) -> do
-        output <- clockErasure initialTime sn -< (time, tagClocks, a)
-        returnA -< CDRight output
-      _ -> error "clockErasure: Impossible pattern in input (Left, Right)/(Right, Left)" -< ()
-clockErasure initialTime (ResamplingHere rb sn) = proc (time, tag, input) -> do
-  case (tag, analyseCD input) of
-    (Left tagL, Left a) -> do
-      b <- eraseClockResBuf LeafProxy LeafProxy initialTime rb -< _
-      returnA -< _
+    (OutClocked clocks)
+clockErasure initialTime (Synchronous clsf sn) = proc (time, tag, input) -> do
+  case (analyseTag tag, analyseIn input) of
+    (Left tagClSF, Left a) -> do
+      b <- eraseClockClSF LeafProxy initialTime clsf -< (time, tagClSF, a)
+      returnA -< OutHere b
+    (Right tagClocks, Right a) -> do
+      output <- clockErasure initialTime sn -< (time, tagClocks, a)
+      returnA -< OutThere output
+    _ -> error "clockErasure: Impossible pattern in input (Left, Right)/(Right, Left)" -< ()
+clockErasure initialTime snAll@(ResamplingHere rb0 sn) = feedback rb0 $ proc ((time, tag, input), rb) -> do
+  (bMaybe, rb') <- case analyseTag tag of
+    Left tagL -> do
+      timeInfo <- withDict (isClocked snAll) $ genTimeInfo LeafProxy initialTime -< (time, tagL)
+      (b, rb') <- arrM $ uncurry get -< (rb, timeInfo)
+      returnA -< (Just b, rb')
+    Right _ -> do
+      returnA -< (Nothing, rb)
+  -- FIXME naming
+  aMaybe <- case (analyseTag <$> analyseTag tag, thereIn <$> thereIn input) of
+    (Right (Right tagRR), Just (Just inputRR)) -> do
+      clockErasure initialTime sn -< (time, TagThere $ TagThere tagRR, InThere $ InThere inputRR)
+    _ -> error "Oh noez" -< ()
+  rb'' <- case (analyseTag <$> analyseTag tag, analyseOut <$> thereOut aMaybe) of
+    (Right (Left tagRL), Just (Left a)) -> do
+      timeInfo <- withDict (isClocked1 snAll) $ genTimeInfo LeafProxy initialTime -< (time, tagRL)
+      arrM $ uncurry $ uncurry put -< ((rb', timeInfo), a)
+    _ -> do
+      returnA -< rb'
+
+  let
+    output = case (analyseTag <$> analyseTag tag, thereOut <$> thereOut aMaybe) of
+      (Left _, _) -> outNow aMaybe
+      (Right (Left _), _) -> OutThere OutNoop
+      (Right (Right _), Just (Just a)) -> OutThere $ OutThere a
+      _ -> error "Wrong"
+  returnA -< (output, rb'')
+clockErasure initialTime (ResamplingSomewhere rb ClosingInHere ClosingOutHere sn) = proc (time, tag, input) -> do
+   _ -< _
+clockErasure _ _ = error "not yet implemented"
+
+isClocked :: SN td ('Clocked a cl b ': clocks) m -> Dict (Clock m cl)
+isClocked (Synchronous _ _) = Dict
+isClocked (ResamplingHere _ sn) = isClocked sn
+isClocked (DoneIn sn) = isClocked sn
+isClocked (DoneOut sn) = isClocked sn
+isClocked (ConcatInNil _) = Dict
+isClocked (ConcatInConsOO sn) = mapDict _ Dict
+isClocked _ = _
+
+-- FIXME Abstract this?
+isClocked1 :: SN td (clocked ': 'Clocked a cl b ': clocks) m -> Dict (Clock m cl)
+isClocked1 (Synchronous _ sn) = isClocked sn
+isClocked1 (ResamplingHere _ sn) = isClocked1 sn
+isClocked1 (DoneIn sn) = isClocked1 sn
+isClocked1 (DoneOut sn) = isClocked1 sn
+isClocked1 (ConcatInNil sn) = isClocked sn
+isClocked1 (ConcatInConsOO sn) = _ -- FIXME I need isClocked2 etc. for this
+isClocked1 _ = _
