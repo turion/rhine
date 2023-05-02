87d9bf01d7610be0fbe6981421be49f17435fa51
Manuel Bärenz <programming@manuelbaerenz.de>
2023-05-02 12:21:35 +0200

    WIP maybe this is RMSMC? Still a time problem

--- rhine-bayes/src/Data/MonadicStreamFunction/Bayes.hs
+++ rhine-bayes/src/Data/MonadicStreamFunction/Bayes.hs
@@ -20,6 +20,8 @@ import Data.MonadicStreamFunction.InternalCore (MSF (..))
 import Control.Monad.Trans.Class
 import Control.Monad.Bayes.Class (MonadDistribution)
 import qualified Control.Monad.Bayes.Traced.Static as Static
+import Control.Monad.Bayes.Sequential.Coroutine (hoistFirst)
+import Control.Monad.Trans.MSF (performOnFirstSample)
 
 -- FIXME rename to sequentialMonteCarlo or smc?
 -- | Run the Sequential Monte Carlo algorithm continuously on an 'MSF'
@@ -67,40 +69,34 @@ resampleMoveSequentialMonteCarlo ::
   -- MSF (t (Population m)) a b ->
   -- FIXME Why not MSF m a (Population b)
   MSF m a [(b, Log Double)]
-resampleMoveSequentialMonteCarlo nParticles nMC resampler = go . (spawn nParticles $>)
+resampleMoveSequentialMonteCarlo nParticles nMC resampler = go . Control.Monad.Bayes.Traced.hoist (spawn nParticles >>) . pure
   where
     go ::
       Monad m =>
-      Population m (MSF (Static.Traced (Population m)) a b) ->
+      Static.Traced (Population m) (MSF (Static.Traced (Population m)) a b) ->
       -- Population m (MSF (t (Population m)) a b) ->
       MSF m a [(b, Log Double)]
     go msfs = MSF $ \a -> do
       -- TODO This is quite different than the dunai version now. Maybe it's right nevertheless.
       -- FIXME This normalizes, which introduces bias, whatever that means
-      bAndMSFs <- runPopulation
-        $ normalize
-        $ Debug.Trace.trace "4"
-        $ marginal
-        $ Debug.Trace.trace "3"
-        $ composeCopies nMC (Debug.Trace.trace "mhStep" mhStep)
-        $ Debug.Trace.trace "2"
-        $ Control.Monad.Bayes.Traced.hoist (Debug.Trace.trace "resampler" resampler)
-        $ Debug.Trace.trace "1"
-        $ flip unMSF (Debug.Trace.trace "a" a) =<< lift (tracePop "msfs" msfs)
-      return $
-        second (go . fromWeightedList . return) $
-          unzip $
-            (swap . fmap fst &&& swap . fmap snd) . swap <$> Debug.Trace.trace "bAndMSFs" bAndMSFs
+      let bAndMSFs = composeCopies nMC mhStep
+            -- $ Debug.Trace.trace "2"
+            $ Control.Monad.Bayes.Traced.hoist (tracePop "after resampler" . Debug.Trace.trace "resampler" resampler . tracePop "before resampler")
+            -- $ Debug.Trace.trace "1"
+            $ flip unMSF (Debug.Trace.trace "a" a) =<< Control.Monad.Bayes.Traced.hoist (tracePop "msfs") msfs
+      bs <- runPopulation $ marginal $ fst <$> bAndMSFs
+      return (bs, go $ snd <$> bAndMSFs)
 
 -- | Apply a function a given number of times.
 composeCopies :: Int -> (a -> a) -> (a -> a)
 composeCopies k f = foldr (.) id (replicate k f)
 
 tracePop :: Monad m => String -> Population m a -> Population m a
-tracePop msg = fromWeightedList . fmap (\pop -> Debug.Trace.traceShow (msg, length pop) pop) . runPopulation
+-- tracePop msg = fromWeightedList . fmap (\pop -> Debug.Trace.traceShow (msg, length pop) pop) . runPopulation
+tracePop _ = id
 
 -- resampleMoveSequentialMonteCarlo nParticles nMC resampler = morphS marginal $ runPopulationS nParticles $ freeze . composeCopies nMC mhStep . hoistTrace resampler
 
 -- FIXME see PR re-adding this to monad-bayes
 normalize :: Monad m => Population m a -> Population m a
-normalize = fromWeightedList . fmap (\particles -> Debug.Trace.traceShow ("length particles", length particles) $ second (/ (sum $ snd <$> particles)) <$> particles) . runPopulation
+normalize = fromWeightedList . fmap (\particles -> second (/ (sum $ snd <$> particles)) <$> particles) . runPopulation
