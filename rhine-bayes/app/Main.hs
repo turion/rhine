c7f89ce1d5d132260fb316444ca98b9575ca00c0
Manuel Bärenz <programming@manuelbaerenz.de>
2023-07-19 17:24:59 +0200

    Introduce StochasticProcess

--- rhine-bayes/app/Main.hs
+++ rhine-bayes/app/Main.hs
@@ -59,12 +59,12 @@ type Sensor = Pos
 
 -- | Harmonic oscillator with white noise
 prior1d ::
-  (MonadDistribution m, Diff td ~ Double) =>
+  (Diff td ~ Double) =>
   -- | Starting position
   Double ->
   -- | Starting velocity
   Double ->
-  BehaviourF m td Temperature Double
+  StochasticProcessF td Temperature Double
 prior1d initialPosition initialVelocity = feedback 0 $ proc (temperature, position') -> do
   impulse <- arrM (normal 0) -< temperature
   let acceleration = (-3) * position' + impulse
@@ -88,11 +88,11 @@ sensorNoiseTemperature :: Double
 sensorNoiseTemperature = 1
 
 -- | A generative model of the sensor noise
-noise :: MonadDistribution m => Behaviour m td Pos
+noise :: StochasticProcess td Pos
 noise = whiteNoise sensorNoiseTemperature &&& whiteNoise sensorNoiseTemperature
 
 -- | A generative model of the sensor position, given the noise
-generativeModel :: (MonadDistribution m, Diff td ~ Double) => BehaviourF m td Pos Sensor
+generativeModel :: (Diff td ~ Double) => StochasticProcessF td Pos Sensor
 generativeModel = proc latent -> do
   noiseNow <- noise -< ()
   returnA -< latent ^+^ noiseNow
