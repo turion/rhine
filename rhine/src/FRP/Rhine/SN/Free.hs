f3104ae1058eef96a0e3bd29f72a02f3c5315f6a
Manuel Bärenz <programming@manuelbaerenz.de>
2023-11-13 15:53:39 +0100

    WIP

--- rhine/src/FRP/Rhine/SN/Free.hs
+++ rhine/src/FRP/Rhine/SN/Free.hs
@@ -1,20 +1,46 @@
 {-# LANGUAGE Arrows #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
-module FRP.Rhine.SN.Free (At (Present), eraseClockSNComponent)
+module FRP.Rhine.SN.Free (
+  At (
+    -- It is intentional _not_ to export the constructor Absent.
+    -- It may only be used internally to guarantee clock safety,
+    -- since otherwise a user could create an absent signal
+    -- in a situation where the clock ticks.
+    -- The constructor Present is harmless though, since an unneeded value is simply discarded.
+    Present
+  ),
+  eraseClockFreeSN,
+  synchronous,
+  resampling,
+  feedbackSN,
+  always,
+  currently,
+  Rhine(..),
+  eraseClockRhine,
+  flow,
+  Clocks(..)
+)
 where
 
 import Control.Arrow.Free
-import Control.Monad.Trans.MSF.Reader (runReaderS, readerS)
+import Control.Monad.Trans.MSF.Reader (readerS, runReaderS)
+import Control.Monad.Trans.Reader (ReaderT, withReaderT)
 import Data.Kind (Constraint, Type)
 import Data.Proxy (Proxy (..))
-import FRP.Rhine (ResamplingBuffer, TimeInfo (TimeInfo))
 import FRP.Rhine.ClSF.Core
-import FRP.Rhine.Clock (Clock (..), TimeDomain, TimeInfo, tag)
+import FRP.Rhine.Clock (Clock (..), TimeDomain, TimeInfo (..), tag)
 import FRP.Rhine.ResamplingBuffer (ResamplingBuffer (..))
+import FRP.Rhine.Clock.Util (genTimeInfo)
+import FRP.Rhine.Clock.Proxy (GetClockProxy(getClockProxy), toClockProxy, ToClockProxy)
+import FRP.Rhine.Schedule (scheduleList)
+import Data.List.NonEmpty (fromList, toList)
+import Control.Monad.Schedule.Class (MonadSchedule)
+import Data.MonadicStreamFunction.Async (concatS)
+import Control.Monad.Trans.MSF (performOnFirstSample)
 import Control.Category (Category)
-import Control.Monad.Trans.Reader (ReaderT, withReaderT)
 
 -- Don't export Absent
 data At cl a = Present !a | Absent
@@ -55,7 +81,7 @@ instance HasClock cl (cl ': cls) where
   project _ (Here ti) = Just ti
   project _ _ = Nothing
 
-instance (HasClock cl cls) => HasClock cl (cl' ': cls) where
+instance {-# OVERLAPPABLE #-} (HasClock cl cls) => HasClock cl (cl' ': cls) where
   inject proxy ti = There $ inject proxy ti
   project _ (Here _) = Nothing
   project proxy (There tick) = project proxy tick
@@ -81,12 +107,36 @@ data SNComponent m cls a b where
     FreeSN m cls (At clB b, c) (At clA a, d) ->
     ResamplingBuffer m clA clB a b ->
     SNComponent m cls c d
-  Wild :: -- FIXME Do I need this really? It's like a general buffer
+  Always :: -- FIXME Do I need this really? It's like a general buffer. So maybe I can get rid of MSF buffer? No, that is still helpful because it has clock constraints
     MSF m a b -> SNComponent m cls a b
 
 newtype FreeSN m cls a b = FreeSN {getFreeSN :: A (SNComponent m cls) a b}
+  deriving (Category, Arrow)
 
-eraseClockSNComponent :: forall m cls a b . (Monad m) => SNComponent m cls a b -> MSF (ReaderT (Tick cls) m) a b
+synchronous :: (HasClock cl cls, Clock m cl) => ClSF m cl a b -> FreeSN m cls (At cl a) (At cl b)
+synchronous = FreeSN . liftFree2 . Synchronous
+
+resampling ::
+  ( HasClock clA cls
+  , Clock m clA
+  , HasClocksOrdered clA clB cls
+  , HasClock clB cls
+  ) =>
+  ResamplingBuffer m clA clB a b ->
+  FreeSN m cls (At clA a) (At clB b)
+resampling = FreeSN . liftFree2 . Resampling
+
+feedbackSN ::
+  (HasClock clA cls, HasClock clB cls) =>
+  FreeSN m cls (At clB b, c) (At clA a, d) ->
+  ResamplingBuffer m clA clB a b ->
+  FreeSN m cls c d
+feedbackSN sn = FreeSN . liftFree2 . Feedback sn
+
+always :: MSF m a b -> FreeSN m cls a b
+always = FreeSN . liftFree2 . Always
+
+eraseClockSNComponent :: forall m cls a b. (Monad m) => SNComponent m cls a b -> MSF (ReaderT (Tick cls) m) a b
 eraseClockSNComponent (Synchronous clsf) = readerS $ proc (tick, a) -> do
   case (project (proxyFromClSF clsf) tick, a) of
     (Nothing, _) -> returnA -< Absent
@@ -95,7 +145,6 @@ eraseClockSNComponent (Synchronous clsf) = readerS $ proc (tick, a) -> do
       returnA -< Present b
     _ -> error "eraseClockSNComponent: Internal error (Synchronous)" -< ()
 eraseClockSNComponent (Resampling resbuf0) = readerS $ eraseClockResBuf (Proxy @cls) resbuf0
-
 eraseClockSNComponent (Feedback sn resbuf0) =
   let
     proxyIn = proxyInFromResBuf resbuf0
@@ -115,8 +164,7 @@ eraseClockSNComponent (Feedback sn resbuf0) =
           arrM $ uncurry $ uncurry put -< ((resbuf', ti), a)
         _ -> error "eraseClockSNComponent: internal error (Resampling)" -< ()
       returnA -< (b, resbuf'')
-
-eraseClockSNComponent (Wild msf) = liftTransS msf
+eraseClockSNComponent (Always msf) = liftTransS msf
 
 eraseClockResBuf ::
   (Monad m, HasClock cla cls, HasClock clb cls) =>
@@ -149,11 +197,15 @@ proxyInFromResBuf _ = Proxy
 proxyOutFromResBuf :: ResamplingBuffer m clA clB a b -> Proxy clB
 proxyOutFromResBuf _ = Proxy
 
-eraseClockFreeSN :: Monad m => FreeSN m cls a b -> MSF (ReaderT (Tick cls) m) a b
+eraseClockFreeSN :: (Monad m) => FreeSN m cls a b -> MSF (ReaderT (Tick cls) m) a b
 eraseClockFreeSN FreeSN {getFreeSN} = runA getFreeSN eraseClockSNComponent
 
-eraseClockFreeSN' :: Monad m => FreeSN m cls a b -> ClSF m (Clocks m td cls) a b
-eraseClockFreeSN' = morphS (withReaderT _) . eraseClockFreeSN
+-- eraseClockFreeSN' :: (Monad m) => FreeSN m cls a b -> ClSF m (Clocks m td cls) a b
+-- eraseClockFreeSN' = morphS (withReaderT _) . eraseClockFreeSN
+
+-- FIXME interesting idea: Erase only some clocks, e.g. the first one of the stack.
+-- Then I need a concept between FreeSN and MSF.
+-- The advantage would be higher flexibility, and I could maye also use MonadSchedule to make the data parts concurrent
 
 type family Map (f :: Type -> Type) (ts :: [Type]) :: [Type] where
   Map f '[] = '[]
@@ -167,11 +219,44 @@ data ClassyClock m td cl where
   ClassyClock :: (Clock m cl, Time cl ~ td) => cl -> ClassyClock m td cl
 
 -- FIXME maybe put Clock constraints and time domain here?
--- data Clocks m td cls where
---   UnitClock :: (Clock m cl, Time cl ~ td) => cl -> Clocks m td '[cl]
---   ConsClocks :: (Clock m cl, Time cl ~ td) => cl -> Clocks m td cls -> Clocks m td (cl ': cls)
-newtype Clocks m td cls = Clocks {getClocks :: HTuple (Map (ClassyClock m td) cls)}
+data Clocks m td cls where
+  UnitClock :: (GetClockProxy cl, Clock m cl, Time cl ~ td) => cl -> Clocks m td '[cl]
+  ConsClocks :: (GetClockProxy cl, Clock m cl, Time cl ~ td) => cl -> Clocks m td cls -> Clocks m td (cl ': cls)
+
+-- FIXME This is
+-- newtype Clocks m td cls = Clocks {getClocks :: HTuple (Map (ClassyClock m td) cls)}
 
 data Tick cls where
   Here :: TimeInfo cl -> Tick (cl ': cls)
   There :: Tick cls -> Tick (cl ': cls)
+
+data Rhine m td cls a b = Rhine
+  { clocks :: Clocks m td cls
+  , freeSN :: FreeSN m cls a b
+  }
+
+eraseClockRhine :: (Monad m, MonadSchedule m) => Rhine m td cls a b -> MSF m a b
+eraseClockRhine Rhine {clocks, freeSN} = proc a -> do
+  ti <- runClocks clocks -< ()
+  runReaderS (eraseClockFreeSN freeSN) -< (ti, a)
+
+flow :: (Monad m, MonadSchedule m) => Rhine m td cls () () -> m ()
+flow = reactimate . eraseClockRhine
+
+runClocks :: (Monad m, MonadSchedule m) => Clocks m td cls -> MSF m () (Tick cls)
+runClocks cls = performOnFirstSample $ scheduleMSFs <$> getRunningClocks cls
+ where
+  getRunningClocks :: Monad m => Clocks m td cls -> m [MSF m () (Tick cls)]
+  getRunningClocks (UnitClock cl) = pure <$> startAndInjectClock cl
+  getRunningClocks (ConsClocks cl cls) = (:) <$> startAndInjectClock cl <*> (map (>>> arr There) <$> getRunningClocks cls)
+
+  startAndInjectClock :: (Monad m, GetClockProxy cl, HasClock cl cls) => Clock m cl => cl -> m (MSF m () (Tick cls))
+  startAndInjectClock cl = do
+    (runningClock, initTime) <- initClock cl
+    return $ runningClock >>> genTimeInfo getClockProxy initTime >>> arr (inject (clockProxy cl))
+
+  clockProxy :: cl -> Proxy cl
+  clockProxy _ = Proxy
+
+  scheduleMSFs :: (Monad m, MonadSchedule m) => [MSF m () a] -> MSF m () a
+  scheduleMSFs msfs = concatS $ scheduleList (fromList msfs) >>> arr toList
