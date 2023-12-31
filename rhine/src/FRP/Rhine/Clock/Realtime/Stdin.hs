23b7ca134745f3eea0bd87d05c2d9dda153e844b
Manuel Bärenz <programming@manuelbaerenz.de>
2023-12-31 18:01:18 +0100

    Change Stdin clock to text

--- rhine/src/FRP/Rhine/Clock/Realtime/Stdin.hs
+++ rhine/src/FRP/Rhine/Clock/Realtime/Stdin.hs
@@ -15,6 +15,10 @@ import Data.Time.Clock
 -- transformers
 import Control.Monad.IO.Class
 
+-- text
+import qualified Data.Text as Text
+import qualified Data.Text.IO as Text
+
 -- rhine
 import FRP.Rhine.Clock
 import FRP.Rhine.Clock.Proxy
@@ -27,13 +31,13 @@ data StdinClock = StdinClock
 
 instance (MonadIO m) => Clock m StdinClock where
   type Time StdinClock = UTCTime
-  type Tag StdinClock = String
+  type Tag StdinClock = Text.Text
 
   initClock _ = do
     initialTime <- liftIO getCurrentTime
     return
       ( constM $ liftIO $ do
-          line <- getLine
+          line <- Text.getLine
           time <- getCurrentTime
           return (time, line)
       , initialTime
