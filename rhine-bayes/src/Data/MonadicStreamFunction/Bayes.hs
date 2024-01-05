218c0a0370ae60524bf88f37393f7b8a20912e98
Manuel Bärenz <programming@manuelbaerenz.de>
2024-01-05 18:47:16 +0100

    Bump monad-bayes to 1.2.0

--- rhine-bayes/src/Data/MonadicStreamFunction/Bayes.hs
+++ rhine-bayes/src/Data/MonadicStreamFunction/Bayes.hs
@@ -24,30 +24,30 @@ runPopulationS ::
   -- | Number of particles
   Int ->
   -- | Resampler
-  (forall x. Population m x -> Population m x) ->
-  MSF (Population m) a b ->
-  -- FIXME Why not MSF m a (Population b)
+  (forall x. PopulationT m x -> PopulationT m x) ->
+  MSF (PopulationT m) a b ->
+  -- FIXME Why not MSF m a (PopulationT b)
   MSF m a [(b, Log Double)]
 runPopulationS nParticles resampler = runPopulationsS resampler . (spawn nParticles $>)
 
--- | Run the Sequential Monte Carlo algorithm continuously on a 'Population' of 'MSF's
+-- | Run the Sequential Monte Carlo algorithm continuously on a 'PopulationT' of 'MSF's
 runPopulationsS ::
   (Monad m) =>
   -- | Resampler
-  (forall x. Population m x -> Population m x) ->
-  Population m (MSF (Population m) a b) ->
+  (forall x. PopulationT m x -> PopulationT m x) ->
+  PopulationT m (MSF (PopulationT m) a b) ->
   MSF m a [(b, Log Double)]
 runPopulationsS resampler = go
   where
     go msfs = MSF $ \a -> do
       -- TODO This is quite different than the dunai version now. Maybe it's right nevertheless.
       -- FIXME This normalizes, which introduces bias, whatever that means
-      bAndMSFs <- runPopulation $ normalize $ resampler $ flip unMSF a =<< msfs
+      bAndMSFs <- runPopulationT $ normalize $ resampler $ flip unMSF a =<< msfs
       return $
         second (go . fromWeightedList . return) $
           unzip $
             (swap . fmap fst &&& swap . fmap snd) . swap <$> bAndMSFs
 
 -- FIXME see PR re-adding this to monad-bayes
-normalize :: (Monad m) => Population m a -> Population m a
-normalize = fromWeightedList . fmap (\particles -> second (/ (sum $ snd <$> particles)) <$> particles) . runPopulation
+normalize :: (Monad m) => PopulationT m a -> PopulationT m a
+normalize = fromWeightedList . fmap (\particles -> second (/ (sum $ snd <$> particles)) <$> particles) . runPopulationT
