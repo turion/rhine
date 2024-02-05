8206cd1a52fbaed5e3e267068852bc749c1436c5
Manuel Bärenz <programming@manuelbaerenz.de>
2024-02-05 15:15:18 +0100

    Add exception tests

--- rhine/test/Except.hs
+++ rhine/test/Except.hs
@@ -25,4 +25,18 @@ tests =
               go $ n - 1
             inputs = [0]
         runScheduleRhinePure (clsf @@ FixedStep @1) inputs @?= [Just 0]
+    , testCase "Can raise, catch, and keep very many exceptions without steps in between" $ do
+        let clsf = safely $ go 1000 []
+            go n ns = do
+              _ <- try $ throwOnCond (< n) () >>> arr (const ns)
+              go (n - 1) (n : ns)
+            inputs = [0]
+        runScheduleRhinePure (clsf @@ FixedStep @1) inputs @?= [Just [1 .. 1000]]
+    , testCase "Can raise, catch, and keep very many exceptions without steps in between, using Monad" $ do
+        let clsf = safely $ go 1000 []
+            go n ns = do
+              n' <- try $ throwOnCond (< n) n >>> arr (const ns)
+              go (n' - 1) (n' : ns)
+            inputs = [0]
+        runScheduleRhinePure (clsf @@ FixedStep @1) inputs @?= [Just [1 .. 1000]]
     ]
