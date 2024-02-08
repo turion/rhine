9618214e035e2cc223ef7ba38c1ad9c655098dc5
Manuel Bärenz <programming@manuelbaerenz.de>
2024-02-08 16:48:17 +0100

    Add Except, Catch, and Single clocks, and many utilities

--- rhine/test/Clock.hs
+++ rhine/test/Clock.hs
@@ -4,12 +4,14 @@ module Clock where
 import Test.Tasty
 
 -- rhine
+import Clock.Except
 import Clock.FixedStep
 import Clock.Millisecond
 
 tests =
   testGroup
     "Clock"
-    [ Clock.FixedStep.tests
+    [ Clock.Except.tests
+    , Clock.FixedStep.tests
     , Clock.Millisecond.tests
     ]
