3492c685405a1be75970c63bec8fa8ed61c309f5
Manuel Bärenz <programming@manuelbaerenz.de>
2024-04-10 16:07:58 +0200

    Refactor word count benchmark

--- rhine/bench/WordCount.hs
+++ rhine/bench/WordCount.hs
@@ -68,13 +68,18 @@ withInput action = do
 
 -- * Frameworks specific implementations of word count
 
+type WordCountClock = (DelayIOError (ExceptClock StdinClock IOError) (Either IOError Int))
+
+wordCountClock :: WordCountClock
+wordCountClock = delayIOError (ExceptClock StdinClock) Left
+
 -- | Idiomatic Rhine implementation with a single clock
 rhineWordCount :: IO Int
 rhineWordCount = do
-  Left (Right count) <- withInput $ runExceptT $ flow $ wc @@ delayIOError (ExceptClock StdinClock) Left
+  Left (Right count) <- withInput $ runExceptT $ flow $ wc @@ wordCountClock
   return count
   where
-    wc :: ClSF (ExceptT (Either IOError Int) IO) (DelayIOError (ExceptClock StdinClock IOError) (Either IOError Int)) () ()
+    wc :: ClSF (ExceptT (Either IOError Int) IO) WordCountClock () ()
     wc = proc _ -> do
       lineOrStop <- tagS -< ()
       words <- mappendS -< either (const 0) (Sum . length . words) lineOrStop
