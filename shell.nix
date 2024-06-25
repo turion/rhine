1e0987e0f483c661fb45c3519a7e00ebd70bee0e
ners <ners@gmx.ch>
2024-06-25 22:01:06 +0200

    Rewrite Nix flake

--- shell.nix
+++
@@ -1,9 +0,0 @@
-let
-  lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-  flake-compat-src = fetchTarball {
-    url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-    sha256 = lock.nodes.flake-compat.locked.narHash;
-  };
-  flake-compat = import flake-compat-src { src =  ./.; };
-in
-flake-compat.shellNix
