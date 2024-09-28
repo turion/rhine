9cbd803b59097267578ca36375b8b751b289c2f2
Manuel Bärenz <programming@manuelbaerenz.de>
2024-09-28 10:54:05 +0200

    Handling composed functors

--- automaton/src/Data/Stream.hs
+++ automaton/src/Data/Stream.hs
@@ -18,7 +18,7 @@ import Prelude hiding (Applicative (..))
 
 -- transformers
 import Control.Monad.Trans.Class
-import Control.Monad.Trans.Except (ExceptT, runExceptT, throwE, withExceptT)
+import Control.Monad.Trans.Except (ExceptT (..), runExceptT, throwE, withExceptT)
 
 -- mmorph
 import Control.Monad.Morph (MFunctor (hoist))
@@ -497,4 +497,31 @@ runListS StreamT {state, step} =
         results <- forM states $ getCompose . step
         let flatResults = concat results
         pure $ Result (resultState <$> flatResults) (output <$> flatResults)
+    }
+
+-- FIXME maybe rewrite with Iso somehow?
+handleCompose :: (Functor f, Monad m, Monad composed) => (forall s. s -> f s) -> (forall x. composed x -> m (f x)) -> (forall x. m (f x) -> composed x) -> StreamT composed a -> StreamT m (f a)
+handleCompose pure_ uncompose compose StreamT {state, step} =
+  StreamT
+    { state = pure_ state
+    , step = \s -> do
+        results <- uncompose $ do
+          states <- compose $ pure s
+          step states
+        pure $! Result (fmap resultState results) (fmap output results)
+    }
+
+-- FIXME all these should go to a separate module
+handleExceptT :: (Monad m) => StreamT (ExceptT e m) a -> StreamT m (Either e a)
+handleExceptT = handleCompose pure runExceptT ExceptT
+
+-- FIXME handleMaybeT
+
+snapshot :: (Functor m) => StreamT m a -> StreamT m (m a)
+snapshot StreamT {state, step} =
+  StreamT
+    { state
+    , step = \s ->
+        let result = step s
+         in flip Result (output <$> result) . resultState <$> result
     }
