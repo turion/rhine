b8e4072ed4a17e0d82a779ca29793ce37c90c200
Manuel Bärenz <Manuel Brenz programming@manuelbaerenz.de>
2024-10-24 17:18:09 +0200

    Improve documentation of exception handling

--- automaton/src/Data/Automaton/Trans/Except.hs
+++ automaton/src/Data/Automaton/Trans/Except.hs
@@ -195,6 +195,24 @@ Here, @automaton@ produces output values of type @b@ until an exception @e1@ occ
 The function @f@ is called on the exception value and produces a continuation automaton
 which is then executed (until it possibly throws an exception @e2@ itself).
 
+Note: By "exceptions", we mean an 'ExceptT' transformer layer, not 'IO' exceptions.
+
+= @do@-Notation
+
+Since the type has a 'Monad' instance, you can use @do@ notation to define exception handling:
+
+@
+example :: Monad m => Automaton m a Int
+example = safely $ do
+  try $ count >>> throwOnMaybe (\n -> guard (n < 10))
+  safe $ arr $ const 0
+@
+
+Here, a counter is incremented until it reaches 10.
+Once it does, an exception is thrown, and the automaton continues to output 0 forever.
+
+= Performance of 'Monad' vs. 'Applicative'
+
 The generality of the monad interface comes at a cost, though.
 In order to achieve higher performance, you should use the 'Monad' interface sparingly.
 Whenever you can express the same control flow using 'Functor', 'Applicative', 'Selective',
@@ -208,7 +226,45 @@ and thus GHC cannot optimize the automaton.
 But often the full expressiveness of '>>=' isn't necessary, and in these cases,
 a much faster automaton is produced by using 'Functor', 'Applicative' and 'Selective'.
 
-Note: By "exceptions", we mean an 'ExceptT' transformer layer, not 'IO' exceptions.
+== Recursive definitions when not using the full 'Monad' interface
+
+When the optimized interface is used (by avoiding '>>='),
+the same caveat as in "Data.Stream" regarding recursive definitions applies:
+They will loop at runtime.
+
+For example, one might expect that this automaton would repeatedly output numbers below or equal to 10:
+
+@
+bad :: Monad m => Automaton m a Int
+bad = safely $ do
+  try $ count >>> throwOnMaybe (\n -> guard (n > 10))
+  safe bad
+@
+
+But in fact it will loop, and never produce output.
+This is because the @do@ notation desugars to an expression involving @>>@, not @>>=@,
+which has higher performance in principle, but doesn't support recursion.
+
+Using the full monad interface misses out on some optimizations, but works:
+
+@
+userSawtooth :: Int -> Automaton IO a Int
+userSawtooth nMax = safely $ do
+  try $ count >>> throwOnMaybe (\n -> guard (n > nMax))
+  nMax' <- once_ $ do
+    putStrLn "Maximum reached, please enter next nMax:"
+    readLn
+  safe $ userSawtooth nMax'
+@
+
+The reason this do notation is desugared using @>>=@ is because the variable @nMax'@ is used later.
+
+To define a recursive, optimized, exception handling automaton, use 'forever':
+
+@
+sawtooth :: Automaton IO a Int
+sawtooth = forever $ try $ count >>> throwOnMaybe (\n -> guard (n > 10))
+@
 -}
 newtype AutomatonExcept a b m e = AutomatonExcept {getAutomatonExcept :: StreamExcept b (ReaderT a m) e}
   deriving newtype (Functor, Applicative, Selective, Monad)
