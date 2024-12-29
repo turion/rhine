2db71982348f97e8d3f088b88ae4a8aaf7668075
Manuel Bärenz <programming@manuelbaerenz.de>
2024-12-29 18:19:31 +0100

    WIP Apply some hints

--- automaton/src/Data/Stream/Except.hs
+++ automaton/src/Data/Stream/Except.hs
@@ -22,7 +22,6 @@ import Control.Selective
 import Data.Stream (foreverExcept)
 import Data.Stream.Optimized as OptimizedStreamT (OptimizedStreamT, applyExcept, constM, hoist', selectExcept)
 import Data.Stream.Optimized qualified as StreamOptimized
-import Data.Stream.Recursive (Recursive (..))
 import Data.Stream.Recursive as Recursive (Recursive (..), hoist')
 import Data.Stream.Recursive.Except
 import Data.Stream.Result
