45d4eab1539f08fec4dd8dc79f218834502f8c33
Manuel Bärenz <programming@manuelbaerenz.de>
2025-03-13 14:58:48 +0100

    WIP

--- automaton/src/Data/Automaton/Schedule.hs
+++ automaton/src/Data/Automaton/Schedule.hs
@@ -1,49 +1,56 @@
-{-# LANGUAGE ExistentialQuantification #-}
-{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE RankNTypes #-}
 
 -- FIXME haddocks
 module Data.Automaton.Schedule where
 
 -- base
 import Control.Arrow
-import Control.Concurrent (forkIO, newEmptyMVar, putMVar, takeMVar, tryTakeMVar, readMVar)
+import Control.Concurrent (forkIO, newEmptyMVar, putMVar, readMVar, takeMVar, tryTakeMVar)
 import Control.Monad (forM_, void)
-import Data.List.NonEmpty as N
+import Control.Monad.IO.Class (MonadIO)
 import Control.Monad.Identity (Identity (..))
 import Data.Function ((&))
 import Data.Functor ((<&>))
 import Data.Functor.Compose (Compose (..))
 import Data.Kind (Type)
-import Control.Monad.IO.Class (MonadIO)
-import Data.Maybe (maybeToList, fromMaybe)
+import Data.List.NonEmpty as N
+import Data.Maybe (fromMaybe, maybeToList)
 
-
-import Data.Foldable1 (Foldable1(foldrMap1))
+import Data.Foldable1 (Foldable1 (foldrMap1))
 
 -- transformers
 import Control.Monad.Trans.Accum (AccumT (..), runAccumT)
+import Control.Monad.Trans.Class (MonadTrans (..))
 import Control.Monad.Trans.Except (ExceptT (..))
 import Control.Monad.Trans.Reader (ReaderT (..))
 import Control.Monad.Trans.Writer.CPS qualified as CPS
 import Control.Monad.Trans.Writer.Strict qualified as Strict
-import Control.Monad.Trans.Class (MonadTrans (..))
 
 -- sop-core
-import Data.SOP (I (..), NP (..), SListI, hzipWith, HSequence (htraverse'), hmap, K (..), HCollapse (hcollapse))
+import Data.SOP (HCollapse (hcollapse), HSequence (htraverse'), I (..), K (..), NP (..), SListI, hmap, hzipWith, unI)
 
 -- free
-import Control.Monad.Trans.Free (FreeT (..), FreeF (..), liftF, iterT)
+import Control.Monad.Trans.Free (FreeF (..), FreeT (..), iterT, liftF)
 
 -- automaton
-import Data.Automaton (Automaton (..), arrM, constM, initialised_, reactimate, withAutomaton_, handleAutomaton, liftS, feedback)
-import Data.Automaton.Trans.Except (exceptS)
+
+import Control.Monad.Schedule.Trans (ScheduleT)
+import Control.Monad.Trans.Changeset (ChangesetT)
+import Data.Automaton (Automaton (..), arrM, constM, feedback, handleAutomaton, initialised_, liftS, reactimate, withAutomaton_)
+import Data.Automaton qualified as Automaton
+import Data.Automaton.Trans.Accum (accumS, runAccumS)
+import Data.Automaton.Trans.Changeset (changesetS, getChangesetS)
+import Data.Automaton.Trans.Except (exceptS, maybeToExceptS)
 import Data.Automaton.Trans.Reader (readerS, runReaderS)
-import Data.Stream ( StreamT(..), concatS )
+import Data.Stream (StreamT (..), concatS)
+import Data.Stream.Optimized (OptimizedStreamT (Stateful), toStreamT)
 import Data.Stream.Result
-import Data.Stream.Optimized (toStreamT, OptimizedStreamT (Stateful))
-import qualified Data.Automaton as Automaton
+import Control.Monad.Trans.Maybe (MaybeT)
+import Data.Automaton.Trans.Maybe (exceptToMaybeS)
+import Data.Automaton.Trans.Writer (runWriterS, writerS)
 
 class MonadSchedule m where
   -- | Run a nonempty list of automata concurrently.
@@ -83,6 +90,9 @@ instance (Monad m, MonadSchedule m) => MonadSchedule (ExceptT e m) where
       >>> schedule
       >>> withAutomaton_ (fmap sequenceA >>> ExceptT)
 
+instance (Monad m, MonadSchedule m) => MonadSchedule (MaybeT m) where
+  schedule = fmap maybeToExceptS >>> schedule >>> exceptToMaybeS
+
 instance (Monoid w, Monad m, MonadSchedule m) => MonadSchedule (CPS.WriterT w m) where
   schedule =
     fmap (withAutomaton_ (CPS.runWriterT >>> fmap (\(Result s a, w) -> Result s (a, w))))
@@ -90,36 +100,39 @@ instance (Monoid w, Monad m, MonadSchedule m) => MonadSchedule (CPS.WriterT w m)
       >>> withAutomaton_ (fmap (\(Result s (a, w)) -> (Result s a, w)) >>> CPS.writerT)
 
 instance (Monoid w, Monad m, MonadSchedule m) => MonadSchedule (Strict.WriterT w m) where
-  schedule =
-    fmap (withAutomaton_ (Strict.runWriterT >>> fmap (\(Result s a, w) -> Result s (a, w))))
-      >>> schedule
-      >>> withAutomaton_ (fmap (\(Result s (a, w)) -> (Result s a, w)) >>> Strict.WriterT)
+  schedule = fmap runWriterS   >>> schedule   >>> writerS
 
 -- FIXME this needs a unit test
 instance (Monoid w, Monad m, MonadSchedule m) => MonadSchedule (AccumT w m) where
-  schedule =
-    fmap (withAutomaton_ (runAccumT >>> ReaderT >>> CPS.writerT))
-      >>> schedule
-      >>> withAutomaton_ (CPS.runWriterT >>> runReaderT >>> AccumT)
-
+  schedule = fmap runAccumS >>> schedule >>> accumS
 
 -- FIXME MaybeT, other WriterT
 instance MonadSchedule Identity where
-  schedule = fmap (getAutomaton >>> toStreamT)
-     >>> foldrMap1 buildStreams consStreams
-    >>> roundRobinStreams >>> fmap N.toList >>> concatS >>> Stateful >>> Automaton
-    where
-      buildStreams :: StreamT m b -> Streams m b
-      buildStreams StreamT {state, step} = Streams
-          { states = I state :* Nil
-          , steps = Step (ResultStateT step) :* Nil
-          }
+  schedule =
+    fmap (getAutomaton >>> toStreamT)
+      >>> nonEmptyStreams
+      >>> roundRobinStreams
+      >>> fmap N.toList
+      >>> concatS
+      >>> Stateful
+      >>> Automaton
 
-      consStreams :: StreamT m b -> Streams m b -> Streams m b
-      consStreams StreamT {state, step} Streams {states, steps} =Streams
-          { states = I state :* states
-          , steps = Step (ResultStateT step) :* steps
-          }
+buildStreams :: StreamT m b -> Streams m b
+buildStreams StreamT {state, step} =
+  Streams
+    { states = I state :* Nil
+    , steps = Step (ResultStateT step) :* Nil
+    }
+
+consStreams :: StreamT m b -> Streams m b -> Streams m b
+consStreams StreamT {state, step} Streams {states, steps} =
+  Streams
+    { states = I state :* states
+    , steps = Step (ResultStateT step) :* steps
+    }
+
+nonEmptyStreams :: NonEmpty (StreamT m b) -> Streams m b
+nonEmptyStreams = foldrMap1 buildStreams consStreams
 
 -- FIXME take care to reverse & test
 
@@ -131,12 +144,14 @@ roundRobinStreams Streams {states, steps} =
         s
           & hzipWith (\Step {getStep} (I s) -> getResultStateT getStep s <&> RunningResult & Compose) steps
           & htraverse' getCompose
-          <&> (\results -> Result
-            (results & hmap (getRunningResult >>> resultState >>> I))
-            (results & hmap (getRunningResult >>> output >>> K) & hnonemptycollapse))
+          <&> ( \results ->
+                  Result
+                    (results & hmap (getRunningResult >>> resultState >>> I))
+                    (results & hmap (getRunningResult >>> output >>> K) & hnonemptycollapse)
+              )
     }
 
-hnonemptycollapse :: SListI as => NP (K b) (a ': as) -> NonEmpty b
+hnonemptycollapse :: (SListI as) => NP (K b) (a ': as) -> NonEmpty b
 hnonemptycollapse (K a :* as) = a :| hcollapse as
 
 -- | A nonempty list of 'StreamT's, unzipped into their states and their steps.
@@ -161,26 +176,30 @@ newtype YieldT m a = YieldT {getYieldT :: FreeT Identity m a}
 type Yield = YieldT Identity
 
 yieldAutomaton :: (Functor m, Monad m) => Automaton (YieldT m) a b -> Automaton m a (Maybe b)
-yieldAutomaton = handleAutomaton $ \StreamT {state, step} -> StreamT
-  {state = step state
-  , step = \s -> ReaderT $ \a -> do
-      oneTick <- runFreeT $ getYieldT $ runReaderT s a
-      return $ case oneTick of
-        Pure (Result s' b) -> Result (step s') (Just b)
-        Free (Identity cont) -> Result (lift $ YieldT cont) Nothing
-  }-- FIXME Could do without do. Or maybe just use applicative do?
+yieldAutomaton = handleAutomaton $ \StreamT {state, step} ->
+  StreamT
+    { state = step state
+    , step = \s -> ReaderT $ \a -> do
+        oneTick <- runFreeT $ getYieldT $ runReaderT s a
+        return $ case oneTick of
+          Pure (Result s' b) -> Result (step s') (Just b)
+          Free (Identity cont) -> Result (lift $ YieldT cont) Nothing
+    } -- FIXME Could do without do. Or maybe just use applicative do?
 
 instance (Monad m, MonadSchedule m) => MonadSchedule (YieldT m) where
   schedule = fmap yieldAutomaton >>> schedule >>> fmap maybeToList >>> Automaton.concatS >>> liftS
 
-yield :: Monad m => YieldT m ()
+yield :: (Monad m) => YieldT m ()
 yield = YieldT $ liftF $ pure ()
 
-runYieldT :: Monad m => YieldT m a -> m a
+runYieldT :: (Monad m) => YieldT m a -> m a
 runYieldT = iterT runIdentity . getYieldT
 
-runYieldTWith :: Monad m => m () -> YieldT m a -> m a
+runYieldTWith :: (Monad m) => m () -> YieldT m a -> m a
 runYieldTWith action = iterT (\ima -> action >> runIdentity ima) . getYieldT
 
 runYield :: Yield a -> a
 runYield = runIdentity . runYieldT
+
+instance (Functor m, MonadSchedule m) => MonadSchedule (ChangesetT s w m) where
+  schedule = fmap getChangesetS >>> schedule >>> changesetS
