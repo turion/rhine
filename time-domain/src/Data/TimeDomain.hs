64444150b0049abb6d8331d7d6fe0c888ac828cf
Manuel Bärenz <programming@manuelbaerenz.de>
2026-04-14 08:42:59 +0200

    Time differences are groups and right actions

--- time-domain/src/Data/TimeDomain.hs
+++ time-domain/src/Data/TimeDomain.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 {- |
 This module defines the 'TimeDomain' class.
@@ -15,8 +19,17 @@ module Data.TimeDomain (
 )
 where
 
+-- base
+import Data.Monoid (Sum (..))
+
 -- time
 import Data.Time.Clock (UTCTime, addUTCTime, diffUTCTime)
+
+-- groups
+import Data.Group (Group (..))
+
+-- changeset
+import Data.Monoid.RightAction (RightAction (actRight), RightTorsor (differenceRight))
 
 {- |
 A time domain is an affine space representing a notion of time,
@@ -28,7 +41,7 @@ Expected laws:
 * @(t `addTime` dt) `diffTime` t = dt@
 * @(t `addTime` dt1) `addTime` dt2 = t `addTime` (dt1 `add` dt2)@
 -}
-class (TimeDifference (Diff time)) => TimeDomain time where
+class (RightAction (Diff time) time, RightTorsor (Diff time) time, TimeDifference (Diff time)) => TimeDomain time where
   -- | The type of differences or durations between two timestamps
   type Diff time
 
@@ -39,9 +52,11 @@ class (TimeDifference (Diff time)) => TimeDomain time where
   @'diffTime' earlier later = later `'diffTime'` earlier@ is the duration it takes from @earlier@ to @later@.
   -}
   diffTime :: time -> time -> Diff time
+  diffTime = differenceRight
 
   -- | Add a time difference to a timestamp.
   addTime :: time -> Diff time -> time
+  addTime = actRight
 
 {- | A type of durations, or differences betweens time stamps.
 
@@ -50,47 +65,65 @@ Expected laws:
 * `add` is commutative and associative
 * @(dt1 `difference` dt2) `add` dt2 = dt1@
 -}
-class TimeDifference d where
+class (Group d) => TimeDifference d where
   {- | Calculate the difference between two durations,
   compatibly with 'diffTime'.
   -}
   difference :: d -> d -> d
+  difference = (~~)
 
   -- | Add two time differences.
   add :: d -> d -> d
+  add = (<>)
 
 -- | Differences between 'UTCTime's are measured in seconds.
 instance TimeDomain UTCTime where
   type Diff UTCTime = Double
-  diffTime t1 t2 = realToFrac $ diffUTCTime t1 t2
-  addTime = flip $ addUTCTime . realToFrac
 
-instance TimeDifference Double where
-  difference = (-)
-  add = (+)
+instance RightAction Double UTCTime where
+  actRight = flip $ addUTCTime . realToFrac
+
+instance RightTorsor Double UTCTime where
+  differenceRight t1 t2 = realToFrac $ diffUTCTime t1 t2
+
+deriving via Sum Double instance Semigroup Double
+deriving via Sum Double instance Monoid Double
+deriving via Sum Double instance Group Double
+deriving via NumTimeDomain Double instance TimeDifference Double
+
+instance RightAction Double Double where
+  actRight = (+)
+instance RightTorsor Double Double where
+  differenceRight = (-)
 
 instance TimeDomain Double where
   type Diff Double = Double
-  diffTime = (-)
-  addTime = (+)
 
-instance TimeDifference Float where
-  difference = (-)
-  add = (+)
+deriving via Sum Float instance Semigroup Float
+deriving via Sum Float instance Monoid Float
+deriving via Sum Float instance Group Float
+deriving via NumTimeDomain Float instance TimeDifference Float
+
+instance RightAction Float Float where
+  actRight = (+)
+instance RightTorsor Float Float where
+  differenceRight = (-)
 
 instance TimeDomain Float where
   type Diff Float = Float
-  diffTime = (-)
-  addTime = (+)
 
-instance TimeDifference Integer where
-  difference = (-)
-  add = (+)
+deriving via Sum Integer instance Semigroup Integer
+deriving via Sum Integer instance Monoid Integer
+deriving via Sum Integer instance Group Integer
+deriving via NumTimeDomain Integer instance TimeDifference Integer
+
+instance RightAction Integer Integer where
+  actRight = (+)
+instance RightTorsor Integer Integer where
+  differenceRight = (-)
 
 instance TimeDomain Integer where
   type Diff Integer = Integer
-  diffTime = (-)
-  addTime = (+)
 
 instance TimeDifference () where
   difference _ _ = ()
@@ -98,18 +131,22 @@ instance TimeDifference () where
 
 instance TimeDomain () where
   type Diff () = ()
-  diffTime _ _ = ()
-  addTime _ _ = ()
 
 -- | Any 'Num' can be wrapped to form a 'TimeDomain'.
 newtype NumTimeDomain a = NumTimeDomain {fromNumTimeDomain :: a}
   deriving (Num)
 
-instance (Num a) => TimeDifference (NumTimeDomain a) where
-  difference = (-)
-  add = (+)
+deriving via Sum (NumTimeDomain a) instance (Num a) => Semigroup (NumTimeDomain a)
+deriving via Sum (NumTimeDomain a) instance (Num a) => Monoid (NumTimeDomain a)
+deriving via Sum (NumTimeDomain a) instance (Num a) => Group (NumTimeDomain a)
+instance (Num a) => TimeDifference (NumTimeDomain a)
+
+-- I would have thought I shouldn't be needing this, but it seems to overlap with the Zip instance for some reason.
+instance {-# OVERLAPPING #-} (Num a) => RightAction (NumTimeDomain a) (NumTimeDomain a) where
+  actRight = (+)
+
+instance (Num a) => RightTorsor (NumTimeDomain a) (NumTimeDomain a) where
+  differenceRight = (-)
 
 instance (Num a) => TimeDomain (NumTimeDomain a) where
   type Diff (NumTimeDomain a) = NumTimeDomain a
-  diffTime = (-)
-  addTime = (+)
