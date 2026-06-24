0c04b586e82787b364d1d01c4d63dfdf7b0e0231
Manuel Bärenz <mbaerenz@heilmannsoftware.de>
2026-06-24 08:42:50 +0200

    Delete now redundant benchmarks

--- rhine/bench/SumMultirate.hs
+++ rhine/bench/SumMultirate.hs
@@ -31,9 +31,6 @@ benchmarks =
     , bench "rhine FixedStep 1000:1" $ whnf rhineFixedStep10001 nMax
     , bench "rhine FixedStep 1000:1 downsampleFixedStep" $ whnf rhineFixedStep11000downsampleFixedStep nMax
     , bench "rhine Trivial Trivial" $ whnf rhineTrivialTrivial nMax
-    , bench "rhine single threaded" $ whnf rhineSingleThreaded nMax
-    , bench "automaton reactimate" $ whnfIO $ automatonReactimate nMax
-    , bench "direct" $ nf direct nMax
     ]
 
 rhineBusyWaitclock :: Int -> IO Int
@@ -130,31 +127,3 @@ rhineTrivialTrivial n =
       if k < n
         then returnA -< ()
         else arrMCl throwE -< s
-
-rhineSingleThreaded :: Int -> Int
-rhineSingleThreaded n =
-  either id absurd $
-    flow $
-      (@@ Trivial) $ proc () -> do
-        k <- count -< ()
-        s <- sumN -< k
-        if k < n
-          then returnA -< ()
-          else arrMCl Left -< s
-
--- This is a single threaded comparison, so expected to be faster
-automatonReactimate :: Int -> IO Int
-automatonReactimate n =
-  fmap (either id absurd) $
-    runExceptT $
-      reactimate $ proc () -> do
-        k <- count -< ()
-        s <- sumN -< k
-        if k < n
-          then returnA -< ()
-          else arrM throwE -< s
-
--- There is no good direct comparison since we can't model the scheduling well
--- So I'll just wastefully create a list of list and concatenate it again... maybe it hits some performance
-direct :: Int -> Int
-direct n = sum $ concatMap pure [0 .. n]
--- rhine/bench/Test.hs
+++ rhine/bench/Test.hs
@@ -14,7 +14,6 @@ wordCount :: Int
 wordCount = 966503
 
 expected = Sum.direct Sum.nMax
-expectedMultirate = SumMultirate.direct SumMultirate.nMax
 
 main :: IO ()
 main =
@@ -34,14 +33,12 @@ main =
           ]
       , testGroup
           "Sum multirate"
-          [ testCase "rhine Busy Waitclock" $ SumMultirate.rhineBusyWaitclock SumMultirate.nMax >>= (@?= expectedMultirate)
-          , testCase "rhine Busy Busy" $ SumMultirate.rhineBusyBusy SumMultirate.nMax >>= (@?= expectedMultirate)
-          , testCase "rhine FixedStep 1:1 IO" $ SumMultirate.rhineFixedStep11IO SumMultirate.nMax >>= (@?= expectedMultirate)
-          , testCase "automaton reactimate" $ SumMultirate.automatonReactimate SumMultirate.nMax >>= (@?= expectedMultirate)
-          , testCase "rhine FixedStep 1000:1" $ SumMultirate.rhineFixedStep10001 SumMultirate.nMax @?= expectedMultirate
-          , testCase "rhine FixedStep 1:1" $ SumMultirate.rhineFixedStep11 SumMultirate.nMax @?= expectedMultirate
-          , testCase "rhine FixedStep 1000:1 downsampleFixedStep" $ SumMultirate.rhineFixedStep11000downsampleFixedStep SumMultirate.nMax @?= expectedMultirate
-          , testCase "rhine Trivial Trivial" $ SumMultirate.rhineTrivialTrivial SumMultirate.nMax @?= expectedMultirate
-          , testCase "rhine single threaded" $ SumMultirate.rhineSingleThreaded SumMultirate.nMax @?= expectedMultirate
+          [ testCase "rhine Busy Waitclock" $ SumMultirate.rhineBusyWaitclock SumMultirate.nMax >>= (@?= expected)
+          , testCase "rhine Busy Busy" $ SumMultirate.rhineBusyBusy SumMultirate.nMax >>= (@?= expected)
+          , testCase "rhine FixedStep 1:1 IO" $ SumMultirate.rhineFixedStep11IO SumMultirate.nMax >>= (@?= expected)
+          , testCase "rhine FixedStep 1000:1" $ SumMultirate.rhineFixedStep10001 SumMultirate.nMax @?= expected
+          , testCase "rhine FixedStep 1:1" $ SumMultirate.rhineFixedStep11 SumMultirate.nMax @?= expected
+          , testCase "rhine FixedStep 1000:1 downsampleFixedStep" $ SumMultirate.rhineFixedStep11000downsampleFixedStep SumMultirate.nMax @?= expected
+          , testCase "rhine Trivial Trivial" $ SumMultirate.rhineTrivialTrivial SumMultirate.nMax @?= expected
           ]
       ]
